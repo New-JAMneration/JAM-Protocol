51c3875a77121c4859b912399c0c6a1ceea15f57
TwEricShen <129346326+TwEricShen@users.noreply.github.com>
2024-12-13 22:25:08 +0800

    chore : fix the format

--- internal/sum/extrinsic/assurance.go
+++ internal/sum/extrinsic/assurance.go
@@ -2,10 +2,10 @@ package extrinsic
 
 // Assurance formula is in GP section 11.2.1   Equation 11.10
 type Assurance struct {
-	Anchor         string // `json:"anchor"`
-	Bitstring      string // `json:"bitfield"`
-	ValidatorIndex uint16 // `json:"validator_index"`
-	Signature      string // `json:"signature"`
+	Anchor         string // json:"anchor"
+	Bitstring      string // json:"bitfield"
+	ValidatorIndex uint16 // json:"validator_index"
+	Signature      string // json:"signature"
 }
 
 // NewAssurance is a constructor, not sure if bitstring should be initialized all zero
