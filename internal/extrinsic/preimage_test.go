46e612ddc9a95f02149cd0776ea9d0e8fab6f7af
Yu Chun Tsao <tsao84672776@gmail.com>
2024-12-13 22:18:29 +0800

    fix: extract remove duplicates, add tests and update comments
    - Extracted the remove duplicates function. (double pointers)
    - Update unit tests.
    - Updated comments.

--- internal/extrinsic/preimage_test.go
+++ internal/extrinsic/preimage_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
+func TestInit(t *testing.T) {
+	var preimages Preimages
+
+	if len(preimages) != 0 {
+		t.Fatalf("Expected 0 preimages, got %d", len(preimages))
+	}
+}
+
 func TestAdd(t *testing.T) {
 	var preimages Preimages
 
@@ -14,58 +22,19 @@ func TestAdd(t *testing.T) {
 	}
 
 	preimages.Add(preimage1)
-
-	if len(preimages) != 1 {
-		t.Fatalf("Expected 1 preimage, got %d", len(preimages))
-	}
 }
 
-func TestSort(t *testing.T) {
+func TestRemoveDuplicatesEmpty(t *testing.T) {
 	var preimages Preimages
 
-	// Add a new Preimage
-	preimage1 := Preimage{
-		Requester: 16909060,
-		Blob:      "0x81095e6122e3bc9d961e00014a7fc833",
-	}
-	preimages.Add(preimage1)
+	preimages.RemoveDuplicates()
 
-	// Add a new Preimage
-	preimage2 := Preimage{
-		Requester: 16909062,
-		Blob:      "0x38db056c7c3065fadb630ce6ccbc7385",
-	}
-	preimages.Add(preimage2)
-
-	// Add a new Preimage
-	preimage3 := Preimage{
-		Requester: 16909061,
-		Blob:      "0xd257bc7d93a55be3561d720d40a6a342",
-	}
-	preimages.Add(preimage3)
-
-	expectedRequesters := []uint32{16909060, 16909061, 16909062}
-	expectedBlobs := []string{
-		"0x81095e6122e3bc9d961e00014a7fc833",
-		"0xd257bc7d93a55be3561d720d40a6a342",
-		"0x38db056c7c3065fadb630ce6ccbc7385",
-	}
-
-	if len(preimages) != len(expectedRequesters) {
-		t.Fatalf("Expected %d preimages, got %d", len(expectedRequesters), len(preimages))
-	}
-
-	for i, preimage := range preimages {
-		if preimage.Requester != expectedRequesters[i] {
-			t.Errorf("Expected requester %d, got %d", expectedRequesters[i], preimage.Requester)
-		}
-		if preimage.Blob != expectedBlobs[i] {
-			t.Errorf("Expected blob %s, got %s", expectedBlobs[i], preimage.Blob)
-		}
+	if len(preimages) != 0 {
+		t.Fatalf("Expected 0 preimages, got %d", len(preimages))
 	}
 }
 
-func TestAddDuplicate(t *testing.T) {
+func TestRemoveDuplicates(t *testing.T) {
 	var preimages Preimages
 
 	// Add a new Preimage
@@ -82,7 +51,31 @@ func TestAddDuplicate(t *testing.T) {
 	}
 	preimages.Add(duplicatePreimage)
 
-	if len(preimages) != 1 {
-		t.Fatalf("Expected 1 preimage, got %d", len(preimages))
+	// Add a new Preimage
+	preimage2 := Preimage{
+		Requester: 16909061,
+		Blob:      "0xd257bc7d93a55be3561d720d40a6a342",
+	}
+	preimages.Add(preimage2)
+
+	if len(preimages) != 2 {
+		t.Fatalf("Expected 2 preimages, got %d", len(preimages))
+	}
+}
+
+func TestSort(t *testing.T) {
+	preimages := Preimages{
+		{Requester: 16909060, Blob: "0x81095e6122e3bc9d961e00014a7fc833"},
+		{Requester: 16909062, Blob: "0x38db056c7c3065fadb630ce6ccbc7385"},
+		{Requester: 16909061, Blob: "0xd257bc7d93a55be3561d720d40a6a342"},
+	}
+
+	preimages.Sort()
+
+	expectedRequesters := []uint32{16909060, 16909061, 16909062}
+	for i, preimage := range preimages {
+		if preimage.Requester != expectedRequesters[i] {
+			t.Errorf("Expected requester %d, got %d", expectedRequesters[i], preimage.Requester)
+		}
 	}
 }
