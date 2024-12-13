a6ef46c772944f5373b223fd2374dfffbf119630
TwEricShen <129346326+TwEricShen@users.noreply.github.com>
2024-12-13 22:42:51 +0800

    Delete internal/sum/extrinsic/assurance.go
    Delete attemp to pass Go Format Check

--- internal/sum/extrinsic/assurance.go
+++
@@ -1,9 +0,0 @@
-package extrinsic
-
-// Assurance formula is in GP section 11.2.1   Equation 11.10
-type Assurance struct {
-	Anchor         string // json:"anchor"
-	Bitstring      string // json:"bitfield"
-	ValidatorIndex uint16 // json:"validator_index"
-	Signature      string // json:"signature"
-}
