d16dab2229c34141869116259829b5a7ea7de0e3
TwEricShen <129346326+TwEricShen@users.noreply.github.com>
2024-12-14 00:39:33 +0800

    feat : add constructor of assurance & add assurance_test

--- internal/extrinsic/assurance.go
+++ internal/extrinsic/assurance.go
@@ -7,3 +7,13 @@ type Assurance struct {
 	ValidatorIndex uint16 `json:"validator_index"`
 	Signature      string `json:"signature"`
 }
+
+// NewAssurance is a constructor, not sure if bitstring should be initialized all zero
+func NewAssurance(anchor string, bitstring string, validatorIndex uint16, signature string) *Assurance {
+	return &Assurance{
+		Anchor:         anchor,
+		Bitstring:      bitstring,
+		ValidatorIndex: validatorIndex,
+		Signature:      signature,
+	}
+}
