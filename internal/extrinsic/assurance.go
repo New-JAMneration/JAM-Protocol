243e046b5755ccbbdb7a5b50f56b093ef0af9782
TwEricShen <129346326+TwEricShen@users.noreply.github.com>
2024-12-13 23:27:14 +0800

    feat : create assurance_extrinsic
    Section 11.2.1
    Equation 11.8
    
    ![image](https://hackmd.io/_uploads/Sk9D13Vm1g.png)

---
+++ internal/extrinsic/assurance.go
@@ -0,0 +1,19 @@
+package extrinsic
+
+// Assurance is in graypaper section 11.2.1 equation 11.10
+type Assurance struct {
+	Anchor         string // json : "anchor"
+	Bitstring      string // json : "bitfield"
+	ValidatorIndex uint16 // json : "validator_index"
+	Signature      string // json : "signature"
+}
+
+// NewAssurance is a constructor, not sure if bitstring should be initialized all zero
+func NewAssurance(anchor string, bitstring string, validatorIndex uint16, signature string) *Assurance {
+	return &Assurance{
+		Anchor:         anchor,
+		Bitstring:      bitstring,
+		ValidatorIndex: validatorIndex,
+		Signature:      signature,
+	}
+}
