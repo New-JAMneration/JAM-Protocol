c3046f19d9ad2abb010a33b6519798153f23c5d4
TwEricShen <129346326+TwEricShen@users.noreply.github.com>
2024-12-14 20:40:28 +0800

    refactor : using jam_types & pre-defined two methods

--- internal/extrinsic/assurance.go
+++ internal/extrinsic/assurance.go
@@ -1,19 +1,19 @@
 package extrinsic
 
-// Assurance equation 11.8
-type Assurance struct {
-	Anchor         string `json:"anchor"`
-	Bitstring      string `json:"bitfield"`
-	ValidatorIndex uint16 `json:"validator_index"`
-	Signature      string `json:"signature"`
+import "github.com/New-JAMneration/JAM-Protocol/internal/jam_types"
+
+// AvailAssurance defined in jam_types
+type AvailAssurance jam_types.AvailAssurance
+
+// SetCoreBit is for setting the validity of the core-index in the assurance bit
+func (a AvailAssurance) SetCoreBit(coreIndex uint16, validity bool) {
+	// equation 11.15 ~ 11.16
+	// 先判定 core 有 pending available 的 report 和 timeout (timeout 在 jam_types line 101 已經處理)
+	// 由 workreport get core-index，並驗證 validity
 }
 
-// NewAssurance is a constructor, not sure if bitstring should be initialized all zero
-func NewAssurance(anchor string, bitstring string, validatorIndex uint16, signature string) *Assurance {
-	return &Assurance{
-		Anchor:         anchor,
-		Bitstring:      bitstring,
-		ValidatorIndex: validatorIndex,
-		Signature:      signature,
-	}
+// GenerateMessage is for generating the message, which is along with the assurance signature
+func GenerateMessage() {
+	// equation 11.13,  currently lack of serialization & encoding & signing context
+
 }
