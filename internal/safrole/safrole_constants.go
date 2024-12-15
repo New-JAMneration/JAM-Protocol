2a8ffc33696d5016b773d5d0cb3d2c78a686cdf5
Peter Peng <peterpeng@KKCompanydeMacBook-Pro.local>
2024-12-15 19:20:29 +0800

    feat: use config for tiny and full const

--- internal/safrole/safrole_constants.go
+++ internal/safrole/safrole_constants.go
@@ -1,6 +1,16 @@
 package safrole
 
-const (
-	ValidatorsCount = 1023
-	EpochLength     = 600
-)
+type Config struct {
+	ValidatorsCount int
+	EpochLength     int
+}
+
+var ConfigFull = Config{
+	ValidatorsCount: 1023,
+	EpochLength:     600,
+}
+
+var ConfigTiny = Config{
+	ValidatorsCount: 6,
+	EpochLength:     12,
+}
