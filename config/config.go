ffa7c4ed854e700e9c40bde9ffb3c7abfc70940c
shank <dear88990@gmail.com>
2024-12-15 20:39:00 +0800

    feat: add config and rm cmd/sum

---
+++ config/config.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/New-JAMneration/JAM-Protocol/internal/jam_types"
+	"github.com/New-JAMneration/JAM-Protocol/logger"
+	"io"
+	"os"
+)
+
+var Config config
+
+type config struct {
+	Log struct {
+		Level string `json:"level"`
+	} `json:"log"`
+	Const struct {
+		ValidatorsCount         int `json:"validators_count"`
+		CoresCount              int `json:"cores_count"`
+		EpochLength             int `json:"epoch_length"`
+		MaxTicketsPerBlock      int `json:"max_tickets_per_block"`
+		TicketsPerValidator     int `json:"tickets_per_validator"`
+		MaxBlocksHistory        int `json:"max_blocks_history"`
+		AuthPoolMaxSize         int `json:"auth_pool_max_size"`
+		AuthQueueSize           int `json:"auth_queue_size"`
+		ValidatorsSuperMajority int `json:"validators_super_majority"`
+		AvailBitfieldBytes      int `json:"avail_bitfield_bytes"`
+	} `json:"const"`
+}
+
+func LoadConfig(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("can't open config file: %w", err)
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("can't read config file: %w", err)
+	}
+
+	if err := json.Unmarshal(bytes, &Config); err != nil {
+		return fmt.Errorf("can't parse config file: %w", err)
+	}
+
+	initJamConst()
+	initLog()
+
+	return nil
+}
+
+func initJamConst() {
+	jam_types.ValidatorsCount = Config.Const.ValidatorsCount
+	jam_types.CoresCount = Config.Const.CoresCount
+	jam_types.EpochLength = Config.Const.EpochLength
+	jam_types.MaxTicketsPerBlock = Config.Const.MaxTicketsPerBlock
+	jam_types.TicketsPerValidator = Config.Const.TicketsPerValidator
+	jam_types.MaxBlocksHistory = Config.Const.MaxBlocksHistory
+	jam_types.AuthPoolMaxSize = Config.Const.AuthPoolMaxSize
+	jam_types.AuthQueueSize = Config.Const.AuthQueueSize
+	jam_types.ValidatorsSuperMajority = Config.Const.ValidatorsSuperMajority
+	jam_types.AvailBitfieldBytes = Config.Const.AvailBitfieldBytes
+}
+
+func initLog() {
+	logger.SetLevel(Config.Log.Level)
+}
--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "github.com/New-JAMneration/JAM-Protocol/cmd"
+import (
+	"flag"
+	"github.com/New-JAMneration/JAM-Protocol/cmd"
+	"github.com/New-JAMneration/JAM-Protocol/config"
+	"os"
+)
 
 func main() {
+	initConfig()
+
 	// 啟動程式
 	cmd.Execute()
+}
+
+func initConfig() {
+	var configPath string
+	set := flag.NewFlagSet("config", flag.ExitOnError)
+	set.StringVar(&configPath, "config", "example.json", "set config file path")
+	err := set.Parse(os.Args[1:])
+	if err != nil {
+		return
+	}
+
+	if err := config.LoadConfig(configPath); err != nil {
+		panic(err)
+	}
 }
