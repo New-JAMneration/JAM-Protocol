72659f12fc893198c77068a9193623b2e2752d9e
Yu Chun Tsao <tsao84672776@gmail.com>
2024-12-18 16:50:34 +0800

    fix: modify types, add test data

--- internal/utilities/hash/hash.go
+++ internal/utilities/hash/hash.go
@@ -3,27 +3,27 @@ package hash
 import (
 	jamTypes "github.com/New-JAMneration/JAM-Protocol/internal/jam_types"
 	"golang.org/x/crypto/blake2b"
-	keecak "golang.org/x/crypto/sha3"
+	"golang.org/x/crypto/sha3"
 )
 
-// blake2bHash generates a hash using the BLAKE2b algorithm.
+// Blake2bHash generates a hash using the BLAKE2b-256 algorithm.
 // The output is a 32-byte hash. (jam_types.OpaqueHash)
-func Blake2bHash(input []byte) jamTypes.OpaqueHash {
+func Blake2bHash(input jamTypes.ByteSequence) jamTypes.OpaqueHash {
 	hash := blake2b.Sum256(input)
 	return jamTypes.OpaqueHash(hash[:])
 }
 
-// blake2bHashPartial generates a hash using the BLAKE2b algorithm for certain x octects.
+// Blake2bHashPartial generates a hash using the BLAKE2b-256 algorithm for certain x octects.
 // The output is a x-byte hash. (jam_types.ByteSequence)
-func Blake2bHashPartial(input []byte, x int) jamTypes.ByteSequence {
+func Blake2bHashPartial(input jamTypes.ByteSequence, x uint) jamTypes.ByteSequence {
 	hash := blake2b.Sum256(input)
 	return jamTypes.ByteSequence(hash[:x])
 }
 
-// KeccakHash generates a hash using the Keccak algorithm.
+// KeccakHash generates a hash using the Keccak-256 algorithm.
 // The output is a 32-byte hash. (jam_types.OpaqueHash)
-func KeccakHash(input []byte) jamTypes.OpaqueHash {
-	hash := keecak.NewLegacyKeccak256()
+func KeccakHash(input jamTypes.ByteSequence) jamTypes.OpaqueHash {
+	hash := sha3.NewLegacyKeccak256()
 	hash.Write(input)
 
 	res := hash.Sum(nil)
