1eab79b019d0af7c9ab420212f3712f793c170ad
Yu Chun Tsao <tsao84672776@gmail.com>
2024-12-19 00:04:06 +0800

    fix: Add bitsToBytes and update test function

--- internal/utilities/merklization/merklization.go
+++ internal/utilities/merklization/merklization.go
@@ -1,6 +1,8 @@
 package merklization
 
 import (
+	"errors"
+
 	"golang.org/x/crypto/blake2b"
 
 	jamTypes "github.com/New-JAMneration/JAM-Protocol/internal/jam_types"
@@ -9,57 +11,74 @@ import (
 
 const NODE_SIZE = 512
 
-// bits converts a byte sequence to a bit sequence.
+// bytesToBits converts a byte sequence to a bit sequence.
 // The function defined in graypaper 3.7.3. Boolean Values
-func bits(s jamTypes.ByteSequence) jamTypes.BitSequence {
+func bytesToBits(s jamTypes.ByteSequence) jamTypes.BitSequence {
 	// Initialize the bit sequence.
-	b := jamTypes.BitSequence{}
+	bitSequence := jamTypes.BitSequence{}
 
 	// Iterate over the byte sequence.
-	for _, v := range s {
+	for _, byte := range s {
 		// Iterate over the bits in the byte.
 		for i := 0; i < 8; i++ {
-			// Append the bit to the bit sequence.
-			boolValue := ((v >> uint(7-i)) & 1) == 1
-			b = append(b, boolValue)
+			// Calculate the bit.
+			bit := ((byte >> uint(7-i)) & 1)
+
+			// Append the bit (bool) to the bit sequence.
+			bitSequence = append(bitSequence, bit == 1)
 		}
 	}
 
 	// Return the bit sequence.
-	return b
+	return bitSequence
+}
+
+// bitsToBytes converts a BitSequence to a ByteSequence
+func bitsToBytes(bits jamTypes.BitSequence) (jamTypes.ByteSequence, error) {
+	if len(bits)%8 != 0 {
+		return nil, errors.New("bit sequence length must be a multiple of 8")
+	}
+
+	byteLength := len(bits) / 8
+	bytes := make(jamTypes.ByteSequence, byteLength)
+
+	for i, bit := range bits {
+		if bit {
+			bytes[i/8] |= 1 << uint(7-i%8)
+		}
+	}
+
+	return bytes, nil
 }
 
 // BranchEncoding encodes a branch node.
 func BranchEncoding(left, right jamTypes.OpaqueHash) jamTypes.BitSequence {
-	var leftByteSequence jamTypes.ByteSequence = left[:]
-	var rightByteSequence jamTypes.ByteSequence = right[:]
+	branchPrefixBit := jamTypes.BitSequence{false}
+	leftBits := bytesToBits(left[:])[1:]
+	rightBits := bytesToBits(right[:])
 
-	branchPrefixBit := jamTypes.BitSequence{false} // prefix with 0, 1 bit
-	leftBits := bits(leftByteSequence)             // left, 255 bits
-	rightBits := bits(rightByteSequence)           // right, 256 bits
+	encoding := jamTypes.BitSequence{}
+	encoding = append(encoding, branchPrefixBit...) // 1 bit
+	encoding = append(encoding, leftBits...)        // 255 bits
+	encoding = append(encoding, rightBits...)       // 256 bits
 
-	encoding := append(branchPrefixBit, append(leftBits[1:], rightBits...)...)
-
-	return encoding
+	return encoding // 512 bits
 }
 
 func embeddedValueLeaf(key jamTypes.OpaqueHash, value jamTypes.ByteSequence) jamTypes.BitSequence {
-	var keyByteSequence jamTypes.ByteSequence = key[:]
-	var valueByteSequence jamTypes.ByteSequence = value[:]
-
 	leftPrefixBit := jamTypes.BitSequence{true}                    // 1 bit
 	embeddedValueLeafPrefixBit := jamTypes.BitSequence{false}      // 1 bit
 	prefix := append(leftPrefixBit, embeddedValueLeafPrefixBit...) // 2 bits
 
 	valueSize := jamTypes.U32(len(value))
 	serializedValueSize := utilities.SerializeFixedLength(valueSize, 1)
-	valueSizeBits := bits(serializedValueSize)
+	valueSizeBits := bytesToBits(serializedValueSize)
 
 	encoding := jamTypes.BitSequence{}
 	encoding = append(encoding, prefix...)
 	encoding = append(encoding, valueSizeBits[:6]...)
-	encoding = append(encoding, bits(keyByteSequence)[:248]...)
-	encoding = append(encoding, bits(valueByteSequence)...)
+	encoding = append(encoding, bytesToBits(key[:])[:248]...)
+	encoding = append(encoding, bytesToBits(value)...)
 
 	// Calculate the size, if it has space left, fill it with 0
 	if len(encoding) < NODE_SIZE {
@@ -70,8 +89,6 @@ func embeddedValueLeaf(key jamTypes.OpaqueHash, value jamTypes.ByteSequence) jam
 }
 
 func regularLeaf(key jamTypes.OpaqueHash, value jamTypes.ByteSequence) jamTypes.BitSequence {
-	var keyByteSequence jamTypes.ByteSequence = key[:]
-
 	leftPrefixBit := jamTypes.BitSequence{true}                                    // 1 bit
 	regularLeafPrefixBit := jamTypes.BitSequence{true}                             // 1 bit
 	fillZeroBits := jamTypes.BitSequence{false, false, false, false, false, false} // 6 bits
@@ -80,9 +97,9 @@ func regularLeaf(key jamTypes.OpaqueHash, value jamTypes.ByteSequence) jamTypes.
 	encoding = append(encoding, leftPrefixBit...)
 	encoding = append(encoding, regularLeafPrefixBit...)
 	encoding = append(encoding, fillZeroBits...)
-	encoding = append(encoding, bits(keyByteSequence)[:248]...)
+	encoding = append(encoding, bytesToBits(key[:])[:248]...)
 	valueHash := blake2b.Sum256(value)
-	encoding = append(encoding, bits(valueHash[:])...)
+	encoding = append(encoding, bytesToBits(valueHash[:])...)
 
 	return encoding
 }
