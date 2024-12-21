986a5521f0ea3a8510c19407dc5242d12313df14
TwEricShen <129346326+TwEricShen@users.noreply.github.com>
2024-12-21 16:27:56 +0800

    feat : create extrinsic_controller, ticket_controller, and dispute_controller

---
+++ internal/extrinsic/extrinsic_controller.go
@@ -0,0 +1,22 @@
+package extrinsic
+
+// ExtrinsicController is a struct that contains all the extrinsics
+// need to take care of the pointers
+type ExtrinsicController struct {
+	TicketController    *TicketController
+	PreimageController  *PreimageController
+	GuaranteeController *GuaranteeController
+	DisputeController   *DisputeController
+	AvailAssurance      *AvailAssuranceController
+}
+
+// NewExtrinsicController returns a new ExtrinsicController
+func NewExtrinsicController(ticketController *TicketController, preimageController *PreimageController,
+	guaranteeController *GuaranteeController, disputeController *DisputeController, availAssurance *AvailAssurance) *ExtrinsicController {
+	return &ExtrinsicController{
+		TicketController:    ticketController,
+		PreimageController:  preimageController,
+		GuaranteeController: guaranteeController,
+		DisputeController:   disputeController,
+		AvailAssurance:      availAssurance,
+
