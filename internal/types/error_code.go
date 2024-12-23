36dcc997e408364fc2fc116f5673e17903dc2435
Peter Peng <peterpeng@KKCompanydeMacBook-Pro.local>
2024-12-23 22:08:12 +0800

    feat: align types of jam_types and safrole

---
+++ internal/types/error_code.go
@@ -0,0 +1,13 @@
+package types
+
+type ErrorCode int
+
+const (
+	BadSlot          ErrorCode = iota // 0 Timeslot value must be strictly monotonic
+	UnexpectedTicket                  // 1 Received a ticket while in epoch's tail
+	BadTicketOrder                    // 2 Tickets must be sorted
+	BadTicketProof                    // 3 Invalid ticket ring proof
+	BadTicketAttempt                  // 4 Invalid ticket attempt value
+	Reserved                          // 5 Reserved
+	DuplicateTicket                   // 6 Found a ticket duplicate
+)
