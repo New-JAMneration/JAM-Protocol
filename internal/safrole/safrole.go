859000ec828641d97f9f0503cabd754a19843cbc
Iapetus <linsc1012@gmail.com>
2024-12-24 21:22:44 +0800

    #106 Add unit tests for #85 and refactor R function
    - Added unit tests to validate the functionality for issue #85.
    - Refactored the R function to improve readability, maintainability, and performance.

--- internal/safrole/safrole.go
+++ internal/safrole/safrole.go
@@ -4,11 +4,16 @@ import (
 	jamTypes "github.com/New-JAMneration/JAM-Protocol/internal/jam_types"
 )
 
-// R function return the epoch and slot index
-// Equation (6.2)
-// !Warning : epoch datatype is undefined in jamtypes and is uncertain
-func R(time U32) (epoch U32, slotIndex U32) {
-	epoch = time / U32(jamTypes.EpochLength)
-	slotIndex = time % U32(jamTypes.EpochLength)
-	return epoch, slotIndex
+// GetEpochIndex returns the epoch index of the most recent block't timeslot
+// \tau : The most recent block't timeslot
+// (6.2)
+func GetEpochIndex(t jamTypes.TimeSlot) jamTypes.TimeSlot {
+	return t / jamTypes.TimeSlot(jamTypes.EpochLength)
+}
+
+// GetSlotIndex returns the slot index of the most recent block't timeslot
+// \tau : The most recent block't timeslot
+// (6.2)
+func GetSlotIndex(t jamTypes.TimeSlot) jamTypes.TimeSlot {
+	return t % jamTypes.TimeSlot(jamTypes.EpochLength)
 }
