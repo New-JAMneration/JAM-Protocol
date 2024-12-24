764460c4a04b0fbc8c6288543aacc974bd1f68dc
Ycc3741 <mo0307b1006@gmail.com>
2024-12-24 12:23:09 +0800

    feat: add store struct and init

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,7 +1,14 @@
 package cmd
 
-import "github.com/New-JAMneration/JAM-Protocol/pkg/cli"
+import (
+    "github.com/New-JAMneration/JAM-Protocol/pkg/cli"
+    "github.com/New-JAMneration/JAM-Protocol/internal/store"
+)
 
 func Execute() {
-	cli.Run()
+    // Initialize global store
+    _ = store.GetInstance()
+    
+    // Run the CLI
+    cli.Run()
 }
