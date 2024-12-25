cd10e1277adc444b2a072f94e7bac85d2ae6f35e
TwEricShen <129346326+TwEricShen@users.noreply.github.com>
2024-12-26 05:29:36 +0800

    feat : eq. 6.25, 6.26

---
+++ internal/safrole/slot_key_sequence.go
@@ -0,0 +1,56 @@
+package safrole
+
+import (
+	types "github.com/New-JAMneration/JAM-Protocol/internal/types"
+	utils "github.com/New-JAMneration/JAM-Protocol/internal/utilities"
+	hash "github.com/New-JAMneration/JAM-Protocol/internal/utilities/hash"
+)
+
+var entropy OpaqueHash
+
+// OutsideInSequencer re-order the slice of ticketsBodies as in GP Eq. 6.25
+func OutsideInSequencer(t *TicketsBodies) []TicketBody {
+	left := 0
+	right := types.EpochLength - 1
+
+	out := make([]TicketBody, types.EpochLength)
+	for i := 0; i < types.EpochLength; i++ {
+		if i%2 == 0 {
+			out[i] = (*t)[left]
+			left++
+		} else {
+			out[i] = (*t)[right]
+			right--
+		}
+	}
+
+	return out
+}
+
+// FallbackKeySequence implements the fallback key sequence in GP Eq. 6.26
+func FallbackKeySequence(entropy types.OpaqueHash, validators types.ValidatorsData) []types.BandersnatchPublic {
+	// require globa variable entropy
+	// type EpochKeys []BandersnatchKey
+	keys := make([]types.BandersnatchPublic, types.EpochLength)
+	var i types.U32
+	var epochLength types.U32 = types.U32(types.EpochLength)
+
+	for i = 0; i < epochLength; i++ {
+		// Get E_4(i)
+		serialized := utils.SerializeFixedLength(i, 4)
+		// Concatenate  entropy with E_4(i)
+		serializedBytes := append(entropy[:], serialized...)
+
+		// H4 : Keccak256(E_4(i) || entropy) according to GP I.3 , I.3 -> See section 3.8 ,
+		// take only the first 4 octets of the hash, 4*8=32 bit
+		hash := hash.KeccakHash(serializedBytes)
+		// E^(-1) deserialization
+		validatorIndex, _ := utils.DeserializeU64(types.ByteSequence(hash[:32]))
+		// validatorIndex : jamtypes.U64
+		validatorIndex %= types.U64(types.ValidatorsCount)
+		// k[]_b : validatorData -> bandersnatch
+		keys[i] = validators[validatorIndex].Bandersnatch
+	}
+
+	return keys
+}
