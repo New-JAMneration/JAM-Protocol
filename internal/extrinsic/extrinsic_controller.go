6a0ef95595f20766136984026dd641be590ce17a
Jake Chen <jake_chen@trendmicro.com>
2024-12-25 15:54:56 +0800

    refactor : rename the variable AvailAssurance, availAssurance and the pointer *AvailAssurance

--- internal/extrinsic/extrinsic_controller.go
+++ internal/extrinsic/extrinsic_controller.go
@@ -3,21 +3,21 @@ package extrinsic
 // ExtrinsicController is a struct that contains all the extrinsics
 // need to take care of the pointers
 type ExtrinsicController struct {
-	TicketController    *TicketController
-	PreimageController  *PreimageController
-	GuaranteeController *GuaranteeController
-	DisputeController   *DisputeController
-	AvailAssurance      *AvailAssuranceController
+	TicketController         *TicketController
+	PreimageController       *PreimageController
+	GuaranteeController      *GuaranteeController
+	DisputeController        *DisputeController
+	AvailAssuranceController *AvailAssuranceController
 }
 
 // NewExtrinsicController returns a new ExtrinsicController
 func NewExtrinsicController(ticketController *TicketController, preimageController *PreimageController,
-	guaranteeController *GuaranteeController, disputeController *DisputeController, availAssurance *AvailAssurance) *ExtrinsicController {
+	guaranteeController *GuaranteeController, disputeController *DisputeController, availAssuranceController *AvailAssuranceController) *ExtrinsicController {
 	return &ExtrinsicController{
-		TicketController:    ticketController,
-		PreimageController:  preimageController,
-		GuaranteeController: guaranteeController,
-		DisputeController:   disputeController,
-		AvailAssurance:      availAssurance,
+		TicketController:         ticketController,
+		PreimageController:       preimageController,
+		GuaranteeController:      guaranteeController,
+		DisputeController:        disputeController,
+		AvailAssuranceController: availAssuranceController,
 	}
 }
