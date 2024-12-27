f4717f778823324be7cd82fe9ab29f2f26b4fa29
Iapetus <linsc1012@gmail.com>
2024-12-28 01:06:43 +0800

    refactor: rename jam_type.go to type.go
    Remove safrole_types.
    Fix ticketbody_controller logic.
    Rename t to tbc.

--- internal/safrole/ticketbody_controller.go
+++ internal/safrole/ticketbody_controller.go
@@ -3,33 +3,37 @@ package safrole
 import (
 	"fmt"
 
+	"github.com/New-JAMneration/JAM-Protocol/internal/types"
 	jamTypes "github.com/New-JAMneration/JAM-Protocol/internal/types"
 )
 
 // (6.5) // (6.6)
 // TicketsBodiesController is a controller for TicketsBodies
 type TicketsBodiesController struct {
-	TicketsBodies []TicketBody
+	TicketsBodies []types.TicketBody
 }
 
 // NewTicketsBodiesController returns a new TicketsBodiesController
 func NewTicketsBodiesController() *TicketsBodiesController {
 	return &TicketsBodiesController{
-		TicketsBodies: make([]TicketBody, 0),
+		TicketsBodies: make([]types.TicketBody, 0),
 	}
 }
 
 // Validate validates the controller
-func (t *TicketsBodiesController) Validate() error {
-	if len(t.TicketsBodies) >= jamTypes.EpochLength {
-		return fmt.Errorf("TicketsBodiesController must have less than %d entries, got %d", jamTypes.EpochLength, len(t.TicketsBodies))
+func (tbc *TicketsBodiesController) Validate() error {
+	if len(tbc.TicketsBodies) > types.EpochLength {
+		return fmt.Errorf("TicketsBodiesController must have less than %d entries, got %d", jamTypes.EpochLength, len(tbc.TicketsBodies))
 	}
 	return nil
 }
 
 // (6.5)
 // AddTicketBody adds a ticket body to the controller
-func (t *TicketsBodiesController) AddTicketBody(ticketBody TicketBody) *TicketsBodiesController {
-	t.TicketsBodies = append(t.TicketsBodies, ticketBody)
-	return t
+func (tbc *TicketsBodiesController) AddTicketBody(ticketBody types.TicketBody) error {
+	if len(tbc.TicketsBodies) < types.EpochLength {
+		tbc.TicketsBodies = append(tbc.TicketsBodies, ticketBody)
+		return nil
+	}
+	return fmt.Errorf("TicketsBodiesController must have less than %d entries, got %d", jamTypes.EpochLength, len(tbc.TicketsBodies))
 }
