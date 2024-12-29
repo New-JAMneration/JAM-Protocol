2dd5d81dd6310e61cbd1f5a582adfc6ed778ac9d
Yu Chun Tsao <tsao84672776@gmail.com>
2024-12-29 16:09:09 +0800

    feat: Implement all header elements
    The state root hash is not implemented yet because the Merklization function has not been merged.

--- internal/header/header_controller.go
+++ internal/header/header_controller.go
@@ -1,7 +1,6 @@
 package header
 
 import (
-	"errors"
 	"fmt"
 	"time"
 
@@ -11,10 +10,14 @@ import (
 	"github.com/New-JAMneration/JAM-Protocol/internal/utilities/hash"
 )
 
+// HeaderController is a controller for the header.
+// This controller is used to manage the header.
+// You can use this controller to create a header.
 type HeaderController struct {
 	Header types.Header
 }
 
+// NewHeaderController creates a new HeaderController.
 func NewHeaderController() *HeaderController {
 	return &HeaderController{
 		Header: types.Header{},
@@ -23,99 +26,163 @@ func NewHeaderController() *HeaderController {
 
 // Set sets the Header to the given Header.
 // You can load the test data and generate a header from this function.
-func (h *HeaderController) Set(header types.Header) {
+func (h *HeaderController) SetHeader(header types.Header) {
 	h.Header = header
 }
 
 // Get returns the Header.
-func (h *HeaderController) Get() types.Header {
+func (h *HeaderController) GetHeader() types.Header {
 	return h.Header
 }
 
-// GetParentHeader returns the parent header of the header.
-// H^- = P(H)
-// (5.2) (5.3)
-func (h *HeaderController) GetParentHeader(header types.Header) (types.Header, error) {
-	s := store.GetInstance()
-	headers := s.GetAncestorHeaders()
-
-	// Use the time slot of the header to get the parent header.
-	if header.Slot == 0 {
-		return types.Header{}, errors.New("This is the genesis header.")
-	}
-
-	// TODO: 僅透過 Slot 來取得 parent header 是否合理?
-	parentSlot := header.Slot - 1
-
-	return headers[parentSlot], nil
-}
-
-// GenerateParentHash generates the parent hash of the header.
-// (5.2) H_p
-func (h *HeaderController) GenerateParentHash(header types.Header) (types.HeaderHash, error) {
-	// P function => get parent header
-	parentHeader, err := h.GetParentHeader(header)
-	if err != nil {
-		fmt.Println(err)
-		return types.HeaderHash{}, err
-	}
-
+// CreateParentHeaderHash creates the parent header hash of the header.
+// (5.2)
+// H_p: parent header hash
+func (h *HeaderController) CreateParentHeaderHash(parentHeader types.Header) {
 	// serialization
 	serializedHeader := utilities.HeaderSerialization(parentHeader)
 
 	// hash function (blake2b)
 	parentHeaderHash := types.HeaderHash(hash.Blake2bHash(serializedHeader))
 
-	return parentHeaderHash, nil
+	h.Header.Parent = parentHeaderHash
 }
 
-func GetCurrentTimeInSecond() int64 {
+// CreateExtrinsicHash creates the extrinsic hash of the header.
+// (5.4), (5.5), (5.6)
+// H_x: extrinsic hash
+func (h *HeaderController) CreateExtrinsicHash(extrinsic types.Extrinsic) {
+	ticketSerialized := hash.Blake2bHash(utilities.ExtrinsicTicketSerialization(extrinsic.Tickets))
+	preimageSerialized := hash.Blake2bHash(utilities.ExtrinsicPreimageSerialization(extrinsic.Preimages))
+	guaranteeSerialized := hash.Blake2bHash(utilities.ExtrinsicGuaranteeSerialization(extrinsic.Guarantees))
+	AssureanceSerialized := hash.Blake2bHash(utilities.ExtrinsicAssuranceSerialization(extrinsic.Assurances))
+	DisputeSerialized := hash.Blake2bHash(utilities.ExtrinsicDisputeSerialization(extrinsic.Disputes))
+
+	// Serialize the hash of the extrinsic elements
+	serializedElements := types.ByteSequence{}
+	serializedElements = append(serializedElements, utilities.WrapByteArray32(types.ByteArray32(ticketSerialized)).Serialize()...)
+	serializedElements = append(serializedElements, utilities.WrapByteArray32(types.ByteArray32(preimageSerialized)).Serialize()...)
+	serializedElements = append(serializedElements, utilities.WrapByteArray32(types.ByteArray32(guaranteeSerialized)).Serialize()...)
+	serializedElements = append(serializedElements, utilities.WrapByteArray32(types.ByteArray32(AssureanceSerialized)).Serialize()...)
+	serializedElements = append(serializedElements, utilities.WrapByteArray32(types.ByteArray32(DisputeSerialized)).Serialize()...)
+
+	// Hash the serialized elements
+	extrinsicHash := types.OpaqueHash(hash.Blake2bHash(serializedElements))
+
+	h.Header.ExtrinsicHash = extrinsicHash
+}
+
+// FIXME: Use the real Jam Common Era
+func getCurrentTimeInSecond() uint64 {
 	// The Jam Common Era is 2025-01-01 12:00:00 UTC defined in the graypaper.
 	// jamCommonEra := time.Date(2025, 1, 1, 12, 0, 0, 0, time.UTC)
 
-	// FIXME: Use the real Jam Common Era
 	jamCommonEra := time.Now().UTC().Add(-600 * time.Second)
 
 	now := time.Now().UTC()
-	secondsSinceJam := int64(now.Sub(jamCommonEra).Seconds())
+	secondsSinceJam := uint64(now.Sub(jamCommonEra).Seconds())
 
 	return secondsSinceJam
 }
 
-// (5.4), (5.5), (5.6)
-// Implement the equation in Appendix C.2 Block Serialization
+func (h *HeaderController) ValidateTimeSlot(parentHeader types.Header, timeslot types.TimeSlot) error {
+	// INFO: Validate the time slot
+	// 1. The time slot of the header is always larger than the parent header's
+	// time slot.
+	// 2. The time slot of the header is always smaller than the current time.
 
-// (5.7)
-// 1. 現在 header 的 time slot 一定在過去的時間點 (H_t * P <= T)
-// 2. 現在 header 的 time slot 一定大於 parent header 的 time slot (P(H)_t <
-// H_t)
-func (h *HeaderController) ValidateHeaderTimeslot(header types.Header) (bool, error) {
-	parentHeader, err := h.GetParentHeader(header)
-	if err != nil {
-		// If the header is the genesis header, continue.
-		return false, err
+	if timeslot <= parentHeader.Slot {
+		return fmt.Errorf("The time slot of the header is always larger than the parent header's time slot.")
 	}
 
-	headerTimeslotGreaterThanParent := header.Slot > parentHeader.Slot
-
-	// TODO: Define the const in types.go or const.go
+	// Get the current time in seconds.
+	currentTimeInSecond := getCurrentTimeInSecond()
 	const slotPeriod uint8 = 6
-	headerTimeSlotInSecond := header.Slot * types.TimeSlot(slotPeriod)
-	headerTimeslotLessThanCurrentTime := int64(headerTimeSlotInSecond) <= GetCurrentTimeInSecond()
+	timeslotInSecond := uint64(timeslot) * uint64(slotPeriod)
 
-	if headerTimeslotGreaterThanParent && headerTimeslotLessThanCurrentTime {
-		return true, nil
+	if timeslotInSecond > currentTimeInSecond {
+		return fmt.Errorf("The time slot of the header is always smaller than the current time.")
 	}
-	return false, nil
+
+	return nil
 }
 
-// (5.8)
-// Implement the equation in Appendix D.2 Merklization
-// TODO: The merklization function is under development.
+// CreateHeaderSlot creates a time slot for the header.
+// (5.7) H_t: time slot
+func (h *HeaderController) CreateHeaderSlot(parentHeader types.Header, timeslot types.TimeSlot) error {
+	err := h.ValidateTimeSlot(parentHeader, timeslot)
+	if err != nil {
+		return err
+	}
 
-// GetAuthor returns the author of the block.
-// (5.9) H_a
-func (h *HeaderController) GetAuthor(authorIndex types.ValidatorIndex) types.Validator {
+	h.Header.Slot = timeslot
+	return nil
+}
+
+// (5.8) H_r: state root hash
+func (h *HeaderController) CreateStateRootHash(state types.State) {
+	// State merklization
+	// FIXME: call function to merklize the state
+	// We're waiting for the merklization function merge to the main branch.
+}
+
+// H_i: a Bandersnatch block author index
+func (h *HeaderController) CreateBlockAuthorIndex(authorIndex types.ValidatorIndex) {
+	h.Header.AuthorIndex = authorIndex
+}
+
+// H_a = k'[H_i]
+// k': posterior current validator set
+func (h *HeaderController) GetAutherBendersnatchKey(header types.Header) types.BandersnatchPublic {
+	authorIndex := header.AuthorIndex
+
+	// Get the posterior current validator set
 	s := store.GetInstance()
-	return s.GetPosteriorValidatorByIndex(authorIndex)
+
+	// Get the validator by index
+	validator := s.GetPosteriorValidatorByIndex(authorIndex)
+
+	return validator.Bandersnatch
+}
+
+// H_e: epoch
+// (5.10)
+func (h *HeaderController) CreateEpochMark(epochMark *types.EpochMark) {
+	h.Header.EpochMark = epochMark
+}
+
+// H_w: winning tickets
+// (5.10)
+func (h *HeaderController) CreateWinningTickets(ticketsMark *types.TicketsMark) {
+	h.Header.TicketsMark = ticketsMark
+}
+
+// H_o: offenders markers
+// (5.10)
+func (h *HeaderController) CreateOffendersMarkers(offendersMark types.OffendersMark) {
+	h.Header.OffendersMark = offendersMark
+}
+
+// H_v: the entropy-yielding VRF signature
+// EntropySource
+func (h *HeaderController) CreateEntropySource(vrfSignature types.BandersnatchVrfSignature) {
+	h.Header.EntropySource = vrfSignature
+}
+
+// H_s: a block seal
+func (h *HeaderController) CreateBlockSeal(blockSeal types.BandersnatchVrfSignature) {
+	h.Header.Seal = blockSeal
+}
+
+// GetParentHeader returns all ancestor headers.
+// (5.3) A
+func (h *HeaderController) GetAncestorHeaders() []types.Header {
+	s := store.GetInstance()
+	return s.GetAncestorHeaders()
+}
+
+// AddAncestorHeader adds the header to the ancestor headers.
+func (h *HeaderController) AddAncestorHeader(header types.Header) {
+	s := store.GetInstance()
+	s.AddAncestorHeader(header)
 }
---
+++ internal/store/intermediateHeaders.go
@@ -0,0 +1,39 @@
+package store
+
+import (
+	"sync"
+
+	"github.com/New-JAMneration/JAM-Protocol/internal/types"
+)
+
+type IntermediateHeader struct {
+	mu     sync.RWMutex
+	Header types.Header
+}
+
+func NewIntermediateHeader() *IntermediateHeader {
+	return &IntermediateHeader{
+		Header: types.Header{},
+	}
+}
+
+// AddHeader adds a header to the intermediateHeaders
+func (i *IntermediateHeader) AddHeader(header types.Header) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.Header = header
+}
+
+// GetHeader returns the intermediateHeader
+func (i *IntermediateHeader) GetHeader() types.Header {
+	i.mu.RLock()
+	defer i.mu.RUnlock()
+	return i.Header
+}
+
+// ResetHeader resets the intermediateHeader
+func (i *IntermediateHeader) ResetHeader() {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.Header = types.Header{}
+}
