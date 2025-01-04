1842a9b156a48dda33629265938722fe48934237
TwEricShen <129346326+TwEricShen@users.noreply.github.com>
2025-01-04 19:37:54 +0800

    feat : eq. 10.7~10.14

--- internal/extrinsic/fault_controller.go
+++ internal/extrinsic/fault_controller.go
@@ -1,8 +1,10 @@
 package extrinsic
 
 import (
+	"bytes"
 	store "github.com/New-JAMneration/JAM-Protocol/internal/store"
 	jamTypes "github.com/New-JAMneration/JAM-Protocol/internal/types"
+	"sort"
 )
 
 // FaultController is a struct that contains a slice of Fault
@@ -26,8 +28,8 @@ func NewFaultController() *FaultController {
 }
 
 func (f *FaultController) VerifyFaultValidity() bool {
-	states := store.GetInstance().GetStates()
-	psiBad := states.GetState().Psi.Bad //  psi_b (bad report) will first update using verdicts in Eq. 10.17
+	postStates := store.GetInstance().GetPosteriorStates()
+	psiBad := postStates.GetState().Psi.Bad //  psi_b (bad report) will first update using verdicts in Eq. 10.17
 
 	f.Faults = f.VerifyReportHashValidty(&psiBad)
 	f.Faults = f.ExcludeOffenders()
@@ -60,7 +62,7 @@ func (f *FaultController) VerifyReportHashValidty(psiBad *[]jamTypes.WorkReportH
 // Offenders []Ed25519Public  `json:"offenders,omitempty"` // Offenders (psi_o)
 func (f *FaultController) ExcludeOffenders() []jamTypes.Fault {
 
-	exclude := store.GetInstance().GetState().Psi.Offenders
+	exclude := store.GetInstance().GetPriorState().Psi.Offenders
 	excludeMap := make(map[jamTypes.Ed25519Public]bool)
 	for _, offenderEd25519 := range exclude {
 		excludeMap[offenderEd25519] = true // true : the offender is in the exclude list
@@ -76,4 +78,32 @@ func (f *FaultController) ExcludeOffenders() []jamTypes.Fault {
 		}
 	}
 	return out
+}
+
+// SortUnique sorts the verdicts and removes duplicates | Eq. 10.8
+func (f *FaultController) SortUnique() {
+	f.Sort()
+	f.Unique()
+}
+
+func (f *FaultController) Unique() {
+	if len(f.Faults) == 0 {
+		return
+	}
+}
+
+func (f *FaultController) Sort() {
+	sort.Sort(f)
+}
+
+func (f *FaultController) Less(i, j int) bool {
+	return bytes.Compare(f.Faults[i].Key[:], f.Faults[j].Key[:]) < 0
+}
+
+func (f *FaultController) Swap(i, j int) {
+	f.Faults[i], f.Faults[j] = f.Faults[j], f.Faults[i]
+}
+
+func (f *FaultController) Len() int {
+	return len(f.Faults)
 }
