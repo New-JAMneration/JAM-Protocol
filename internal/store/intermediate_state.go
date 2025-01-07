4744368d860effd848bea51adfa6e6e32839fc27
Peter Peng <peterpeng@KKCompanydeMacBook-Pro.local>
2025-01-08 00:03:52 +0800

    fix: add IntermediateState struct with RhoDagger

--- internal/store/intermediate_state.go
+++ internal/store/intermediate_state.go
@@ -7,24 +7,30 @@ import (
 )
 
 type IntermediateStates struct {
-	mu        sync.RWMutex
-	RhoDagger *jamTypes.AvailabilityAssignments
+	mu    sync.RWMutex
+	state *IntermediateState
+}
+
+type IntermediateState struct {
+	RhoDagger jamTypes.AvailabilityAssignments
 }
 
 func NewIntermediateStates() *IntermediateStates {
 	return &IntermediateStates{
-		RhoDagger: &jamTypes.AvailabilityAssignments{},
+		state: &IntermediateState{
+			RhoDagger: jamTypes.AvailabilityAssignments{},
+		},
 	}
 }
 
 func (s *IntermediateStates) GetRhoDagger() jamTypes.AvailabilityAssignments {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
-	return *s.RhoDagger
+	return s.state.RhoDagger
 }
 
 func (s *IntermediateStates) SetRhoDagger(rhoDagger jamTypes.AvailabilityAssignments) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	s.RhoDagger = &rhoDagger
+	s.state.RhoDagger = rhoDagger
 }
