17e390825af71f066ae4b5f0563e404fb21fd51b
Peter Peng <peterpeng@KKCompanydeMacBook-Pro.local>
2025-01-07 19:33:34 +0800

    feat: set Psi in posterior state

--- internal/store/posterior_state.go
+++ internal/store/posterior_state.go
@@ -28,3 +28,27 @@ func (s *PosteriorStates) SetRho(rho jamTypes.AvailabilityAssignments) {
 	defer s.mu.Unlock()
 	s.state.Rho = rho
 }
+
+func (s *PosteriorStates) SetPsiG(psiG []jamTypes.WorkReportHash) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state.Psi.Good = psiG
+}
+
+func (s *PosteriorStates) SetPsiB(psiB []jamTypes.WorkReportHash) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state.Psi.Bad = psiB
+}
+
+func (s *PosteriorStates) SetPsiW(psiW []jamTypes.WorkReportHash) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state.Psi.Wonky = psiW
+}
+
+func (s *PosteriorStates) SetPsiO(psiO []jamTypes.Ed25519Public) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state.Psi.Offenders = psiO
+}
