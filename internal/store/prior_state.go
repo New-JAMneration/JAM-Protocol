b65d774b4c5d23eb32a0987e8d08449a8ad6a264
Peter Peng <peterpeng@KKCompanydeMacBook-Pro.local>
2025-01-13 03:40:46 +0800

    feat: set prior state Psi and Rho

--- internal/store/prior_state.go
+++ internal/store/prior_state.go
@@ -52,3 +52,33 @@ func (s *PriorStates) AddPsiOffenders(offender types.Ed25519Public) {
 	defer s.mu.Unlock()
 	s.state.Psi.Offenders = append(s.state.Psi.Offenders, offender)
 }
+
+func (s *PriorStates) SetPsiG(psiG []types.WorkReportHash) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state.Psi.Good = psiG
+}
+
+func (s *PriorStates) SetPsiB(psiB []types.WorkReportHash) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state.Psi.Bad = psiB
+}
+
+func (s *PriorStates) SetPsiW(psiW []types.WorkReportHash) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state.Psi.Wonky = psiW
+}
+
+func (s *PriorStates) SetPsiO(psiO []types.Ed25519Public) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state.Psi.Offenders = psiO
+}
+
+func (s *PriorStates) SetRho(rho types.AvailabilityAssignments) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.state.Rho = rho
+}
