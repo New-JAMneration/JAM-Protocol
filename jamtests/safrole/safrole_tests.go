6c8d8ec214ce1026461d673f15c8faee43a07b96
TwEricShen <129346326+TwEricShen@users.noreply.github.com>
2025-01-16 03:03:08 +0800

    refactor : jam test types

---
+++ jamtests/safrole/safrole_tests.go
@@ -0,0 +1,53 @@
+package jamtests
+
+import (
+	"github.com/New-JAMneration/JAM-Protocol/internal/types"
+)
+
+type SafroleTestCase struct {
+	Input     SafroleInput  `json:"input"`
+	PreState  SafroleState  `json:"pre_state"`
+	Output    SafroleOutput `json:"output"`
+	PostState SafroleState  `json:"post_state"`
+}
+
+type SafroleInput struct {
+	Slot      types.TimeSlot         `json:"slot"`      // Current slot
+	Entropy   types.Entropy          `json:"entropy"`   // Per block entropy (originated from block entropy source VRF)
+	Extrinsic types.TicketsExtrinsic `json:"extrinsic"` // Safrole extrinsic
+}
+
+type SafroleOutputData struct {
+	EpochMark   *types.EpochMark   `json:"epoch_mark,omitempty"`   // New epoch marker (optional).
+	TicketsMark *types.TicketsMark `json:"tickets_mark,omitempty"` // Winning tickets marker (optional).
+}
+
+type SafroleErrorCode types.ErrorCode
+
+type SafroleOutput struct {
+	Ok  *SafroleOutputData `json:"ok,omitempty"`
+	Err *SafroleErrorCode  `json:"err,omitempty"`
+}
+
+type SafroleState struct {
+	Tau           types.TimeSlot                   `json:"tau"`            // Most recent block's timeslot
+	Eta           types.EntropyBuffer              `json:"eta"`            // Entropy accumulator and epochal randomness
+	Lambda        types.ValidatorsData             `json:"lambda"`         // Validator keys and metadata which were active in the prior epoch
+	Kappa         types.ValidatorsData             `json:"kappa"`          // Validator keys and metadata currently active
+	GammaK        types.ValidatorsData             `json:"gamma_k"`        // Validator keys for the following epoch
+	Iota          types.ValidatorsData             `json:"iota"`           // Validator keys and metadata to be drawn from next
+	GammaA        types.TicketsAccumulator         `json:"gamma_a"`        // Sealing-key contest ticket accumulator
+	GammaS        types.TicketsOrKeys              `json:"gamma_s"`        // Sealing-key series of the current epoch
+	GammaZ        types.BandersnatchRingCommitment `json:"gamma_z"`        // Bandersnatch ring commitment
+	PostOffenders []types.Ed25519Public            `json:"post_offendors"` // Posterior offenders sequence
+}
+
+const (
+	BadSlot          SafroleErrorCode = iota // 0 Timeslot value must be strictly monotonic
+	UnexpectedTicket                         // 1 Received a ticket while in epoch's tail
+	BadTicketOrder                           // 2 Tickets must be sorted
+	BadTicketProof                           // 3 Invalid ticket ring proof
+	BadTicketAttempt                         // 4 Invalid ticket attempt value
+	Reserved                                 // 5 Reserved
+	DuplicateTicket                          // 6 Found a ticket duplicate
+)
