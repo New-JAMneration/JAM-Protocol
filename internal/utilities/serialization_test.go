3b1e9d5653f7567f3af470c51ce0e5a6069e5164
terryhung <terryhung1228@gmail.com>
2025-01-15 15:15:56 +0800

    fix: simplified test

--- internal/utilities/serialization_test.go
+++ internal/utilities/serialization_test.go
@@ -289,8 +289,8 @@ func TestWrapOpaqueHash(t *testing.T) {
 func TestWrapOpaqueHashMap(t *testing.T) {
 	// Mock inputs
 	input := map[types.OpaqueHash]types.ByteSequence{
-		types.OpaqueHash{7, 8, 9}: {10, 11, 12},
-		types.OpaqueHash{1, 2, 3}: {4, 5, 6},
+		{7, 8, 9}: {10, 11, 12},
+		{1, 2, 3}: {4, 5, 6},
 	}
 
 	// Expected outputs
