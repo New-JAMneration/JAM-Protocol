2026f2893c21b445a4aeb7ad16982137e08926a0
Yu Chun Tsao <tsao84672776@gmail.com>
2025-02-09 23:22:01 +0800

    fix: Passed reports tests and updated jamtests types
    - Followed davxy/jam-test-vectors to update jamtests types
    - Passed reports test cases

--- internal/utilities/encoder_test.go
+++ internal/utilities/encoder_test.go
@@ -14,7 +14,7 @@ import (
 	// jamtests_disputes "github.com/New-JAMneration/JAM-Protocol/jamtests/disputes"
 	// jamtests_history "github.com/New-JAMneration/JAM-Protocol/jamtests/history"
 	// jamtests_preimages "github.com/New-JAMneration/JAM-Protocol/jamtests/preimages"
-	// jamtests_reports "github.com/New-JAMneration/JAM-Protocol/jamtests/reports"
+	jamtests_reports "github.com/New-JAMneration/JAM-Protocol/jamtests/reports"
 	jamtests_safrole "github.com/New-JAMneration/JAM-Protocol/jamtests/safrole"
 	jamtests_statistics "github.com/New-JAMneration/JAM-Protocol/jamtests/statistics"
 )
@@ -219,7 +219,47 @@ func TestEncodeJAMTestSafrole(t *testing.T) {
 			t.Errorf("Failed to load test case from %s: %v", jsonFile, err)
 		}
 
-		data = data.(jamtests_safrole.SafroleTestCase)
+		// Encode the data
+		encoder := NewEncoder()
+		encodeResult, err := encoder.Encode(data)
+		if err != nil {
+			t.Errorf("Failed to encode test case from %s: %v", jsonFile, err)
+		}
+
+		// Load the bin file
+		filename := jsonFile[:len(jsonFile)-len(jsonExtention)]
+		binFile := filename + binExtention
+		binData, err := LoadJAMTestBinaryCase(dir + binFile)
+
+		// compare the encoded data with the binary data
+		if string(encodeResult) != string(binData) {
+			t.Errorf("encoded data does not match the binary data")
+		}
+
+		fmt.Println("✅", "[", mode, "]", filename)
+	}
+}
+
+func TestEncodeJAMTestReport(t *testing.T) {
+	mode := "full" // tiny or full
+	dir := "../../pkg/test_data/jam-test-vectors/reports/" + mode + "/"
+	jsonExtention := ".json"
+	binExtention := ".bin"
+
+	// Get json files
+	jsonFiles, err := getTargetExtensionFiles(dir, jsonExtention)
+	if err != nil {
+		t.Errorf("Failed to get json files: %v", err)
+	}
+
+	// Read the json files
+	for _, jsonFile := range jsonFiles {
+		// Load the json file
+		filePath := dir + jsonFile
+		data, err := LoadJAMTestJsonCase(filePath, reflect.TypeOf(jamtests_reports.ReportsTestCase{}))
+		if err != nil {
+			t.Errorf("Failed to load test case from %s: %v", jsonFile, err)
+		}
 
 		// Encode the data
 		encoder := NewEncoder()
