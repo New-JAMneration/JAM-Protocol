d8b2a9d1121c8e42886a3f0db7ecd4d8e2994e53
yu2C <c2001413@gmail.com>
2025-02-23 03:43:44 +0800

    solve conflict

--- PolkaVM/invocation.go
+++ PolkaVM/invocation.go
@@ -5,11 +5,7 @@ import "fmt"
 // SingleStepInvoke is A.1 (v0.6.2)
 func SingleStepInvoke(programBlob []byte, programCounter ProgramCounter,
 	gas Gas, registers Registers, memory Memory) (
-<<<<<<< HEAD
 	error, ProgramCounter, Gas, Registers, Memory,
-=======
-	error, uint32, Gas, Registers, Memory,
->>>>>>> c9111418debd2bbac686b509e7977bca386c3f05
 ) {
 	// deblob programCodeBlob (c, k, j)  A.2
 	programCodeBlob, err := DeBlobProgramCode(programBlob)
@@ -17,14 +13,9 @@ func SingleStepInvoke(programBlob []byte, programCounter ProgramCounter,
 		return PVMExitTuple(PANIC, nil), programCounter, gas, registers, memory
 	}
 
-<<<<<<< HEAD
 	var exitReason error
-	exitReason, programCounter, gas, registers, memory = SingleStepStateTransition(
-		programCodeBlob.InstructionData, programCodeBlob.Bitmasks,
-=======
 	exitReason, programCounterPrime, gasPrime, registersPrime, memoryPrime := SingleStepStateTransition(
-		programBlob, programCodeBlob.Bitmasks,
->>>>>>> c9111418debd2bbac686b509e7977bca386c3f05
+		programCodeBlob.InstructionData, programCodeBlob.Bitmasks,
 		programCodeBlob.JumpTables, programCounter, gas,
 		registers, memory)
 
@@ -34,17 +25,10 @@ func SingleStepInvoke(programBlob []byte, programCounter ProgramCounter,
 
 	switch exitReason {
 	case PVMExitTuple(CONTINUE, nil):
-<<<<<<< HEAD
-		SingleStepInvoke(programBlob, programCounter, gas, registers, memory)
-	case PVMExitTuple(HALT, nil):
-	case PVMExitTuple(PANIC, nil):
-		return PVMExitTuple(PANIC, nil), 0, gas, registers, memory
-=======
 		SingleStepInvoke(programBlob, programCounterPrime, gasPrime, registersPrime, memoryPrime)
 	case PVMExitTuple(HALT, nil):
 	case PVMExitTuple(PANIC, nil):
 		return exitReason, 0, gasPrime, registersPrime, memoryPrime
->>>>>>> c9111418debd2bbac686b509e7977bca386c3f05
 	default:
 		return exitReason, programCounterPrime, gasPrime, registersPrime, memoryPrime
 	}
@@ -54,7 +38,6 @@ func SingleStepInvoke(programBlob []byte, programCounter ProgramCounter,
 
 // (v.6.2 A.6, A.7) SingleStepStateTransition
 func SingleStepStateTransition(instructionCode []byte, bitmask []byte, jumpTable []uint64,
-<<<<<<< HEAD
 	programCounter ProgramCounter, gas Gas, registers Registers, memory Memory) (
 	error, ProgramCounter, Gas, Registers, Memory,
 ) {
@@ -73,14 +56,4 @@ func SingleStepStateTransition(instructionCode []byte, bitmask []byte, jumpTable
 
 	fmt.Println("run opcode", opcode)
 	return exitReason, programCounter, gas, registers, memory
-=======
-	programCounter uint32, gas Gas, registers Registers, memory Memory) (
-	error, uint32, Gas, Registers, Memory,
-) {
-	// TODO : execute instructions and return PVM state with prime
-	var err error
-	programCounter += uint32(1) + skip(int(programCounter), bitmask)
-
-	return err, programCounter, gas, registers, memory
->>>>>>> c9111418debd2bbac686b509e7977bca386c3f05
 }
