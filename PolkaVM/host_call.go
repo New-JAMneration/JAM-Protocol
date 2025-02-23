7983416cf87666571ecd96d6b6d38f2854b3bd84
HanaYukii <islu245777@gmail.com>
2025-02-23 22:35:47 +0800

    Update

--- PolkaVM/host_call.go
+++ PolkaVM/host_call.go
@@ -3,24 +3,20 @@ package PolkaVM
 import "fmt"
 
 type Psi_H_ReturnType struct {
-	Pagefault        bool            // exit reason is page fault (for handling multiple return type)
-	ExitReason       ExitReasonTypes // exit reason
-	Counter          uint64          // new instruction counter
-	Gas              Gas             // gas remain
-	Reg              Registers       // new registers
-	Ram              Memory          // new memory
-	Addition         any             // addition host-call context
-	PagefaultAddress uint64          // page fault address, only use for page fault
+	ExitReason error     // exit reason
+	Counter    uint64    // new instruction counter
+	Gas        Gas       // gas remain
+	Reg        Registers // new registers
+	Ram        Memory    // new memory
+	Addition   any       // addition host-call context
 }
 
 type OmegaReturnType struct {
-	Pagefault        bool            // exit reason is page fault (for handling multiple return type)
-	ExitReason       ExitReasonTypes // exit reason
-	GasRemain        Gas             // gas remain
-	Register         Registers       // new registers
-	Ram              Memory          // new memory
-	Addition         any             // addition host-call context
-	PagefaultAddress uint64          // page fault address, only use for page fault
+	ExitReason error     // exit reason
+	GasRemain  Gas       // gas remain
+	Register   Registers // new registers
+	Ram        Memory    // new memory
+	Addition   any       // addition host-call context
 }
 
 // (A.31) Ψ_H
@@ -39,8 +35,7 @@ func Psi_H(
 	fmt.Println(exitreason_prime, counter_prime, gas_prime, reg_prime, memory_prime)
 	reason := exitreason_prime.(*PVMExitReason)
 	if reason.Reason == HALT || reason.Reason == PANIC || reason.Reason == OUT_OF_GAS || reason.Reason == PAGE_FAULT {
-		psi_result.Pagefault = false
-		psi_result.ExitReason = reason.Reason
+		psi_result.ExitReason = PVMExitTuple(reason.Reason, nil)
 		psi_result.Counter = uint64(counter_prime)
 		psi_result.Gas = gas_prime
 		psi_result.Reg = reg_prime
@@ -48,20 +43,18 @@ func Psi_H(
 		psi_result.Addition = addition
 	} else if reason.Reason == HOST_CALL {
 		omega_result := omega(*reason.FaultAddr, gas_prime, reg_prime, ram, addition)
-		if omega_result.Pagefault {
-			psi_result.Pagefault = true
-			psi_result.PagefaultAddress = omega_result.PagefaultAddress
+		omega_reason := omega_result.ExitReason.(*PVMExitReason)
+		if omega_reason.Reason == PAGE_FAULT {
 			psi_result.Counter = uint64(counter_prime)
 			psi_result.Gas = gas_prime
 			psi_result.Reg = reg_prime
 			psi_result.Ram = memory_prime
-			psi_result.ExitReason = PAGE_FAULT
+			psi_result.ExitReason = PVMExitTuple(PAGE_FAULT, *omega_reason.FaultAddr)
 			psi_result.Addition = addition
-		} else if omega_result.ExitReason == CONTINUE {
+		} else if omega_reason.Reason == CONTINUE {
 			return Psi_H(code, ProgramCounter(counter_prime), omega_result.GasRemain, omega_result.Register, omega_result.Ram, omega, omega_result.Addition)
 			// NEED TO USE SKIP??
-		} else if omega_result.ExitReason == PANIC || omega_result.ExitReason == OUT_OF_GAS || omega_result.ExitReason == HALT {
-			psi_result.Pagefault = false
+		} else if omega_reason.Reason == PANIC || omega_reason.Reason == OUT_OF_GAS || omega_reason.Reason == HALT {
 			psi_result.ExitReason = omega_result.ExitReason
 			psi_result.Counter = uint64(counter_prime)
 			psi_result.Gas = omega_result.GasRemain
