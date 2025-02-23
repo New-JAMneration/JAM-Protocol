3d5e8f128f13f9f2ef496d4dba01cadbcedb89c7
Yu Chun Tsao <tsao84672776@gmail.com>
2025-02-23 22:53:50 +0800

    feat: Implemented encode function to pass accumulate test cases

--- internal/types/encoder_test.go
+++ internal/types/encoder_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/New-JAMneration/JAM-Protocol/internal/types"
+	jamtests_accmuluate "github.com/New-JAMneration/JAM-Protocol/jamtests/accumulate"
 	jamtests_assurances "github.com/New-JAMneration/JAM-Protocol/jamtests/assurances"
 	jamtests_authorizations "github.com/New-JAMneration/JAM-Protocol/jamtests/authorizations"
 	jamtests_disputes "github.com/New-JAMneration/JAM-Protocol/jamtests/disputes"
@@ -383,6 +384,54 @@ func TestEncodeJamTestVectorsAuthorizations(t *testing.T) {
 		// read json file
 		jsonFilePath := filepath.Join(dir, jsonFile)
 		structType := reflect.TypeOf(jamtests_authorizations.AuthorizationTestCase{})
+		data, err := LoadJAMTestJsonCase(jsonFilePath, structType)
+		if err != nil {
+			t.Fatalf("Failed to read JSON file: %v", err)
+		}
+
+		structValue := reflect.New(structType).Elem()
+		structValue.Set(reflect.ValueOf(data))
+
+		// Encode the JSON data
+		encoder := types.NewEncoder()
+		encoded, err := encoder.Encode(structValue.Addr().Interface())
+		if err != nil {
+			t.Fatalf("Failed to encode JSON data: %v", err)
+		}
+
+		// Read binary file
+		filename := jsonFile[:len(jsonFile)-len(JSON_EXTENTION)]
+		binFileName := GetBinFilename(filename)
+		binFilePath := filepath.Join(dir, binFileName)
+		binData, err := LoadJAMTestBinaryCase(binFilePath)
+		if err != nil {
+			t.Fatalf("Failed to read binary file: %v", err)
+		}
+
+		// Compare the binary data
+		if !CompareBinaryData(encoded, binData) {
+			log.Printf("❌ [%s] %s", types.TEST_MODE, filename)
+			t.Fatalf("Binary data is not equal to the expected data")
+		} else {
+			log.Printf("✅ [%s] %s", types.TEST_MODE, filename)
+		}
+	}
+}
+
+// Accumulate
+func TestEncodeJamTestVectorsAccumulate(t *testing.T) {
+	dir := filepath.Join(JAM_TEST_VECTORS_DIR, "accumulate", types.TEST_MODE)
+
+	// Read json files
+	jsonFiles, err := GetTargetExtensionFiles(dir, JSON_EXTENTION)
+	if err != nil {
+		t.Errorf("Failed to get JSON files: %v", err)
+	}
+
+	for _, jsonFile := range jsonFiles {
+		// read json file
+		jsonFilePath := filepath.Join(dir, jsonFile)
+		structType := reflect.TypeOf(jamtests_accmuluate.AccumulateTestCase{})
 		data, err := LoadJAMTestJsonCase(jsonFilePath, structType)
 		if err != nil {
 			t.Fatalf("Failed to read JSON file: %v", err)
