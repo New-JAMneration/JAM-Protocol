e23f28ea806728a308e7d1b13b76804437c07d9b
Alex Tsay <alex.m.tsay@gmail.com>
2025-03-01 22:54:06 +0800

    feat: Add djump and branch instruction execution.
    This commit adds some common code to share with other
    concurrent developments.

---
+++ PolkaVM/branch.go
@@ -0,0 +1,9 @@
+package PolkaVM
+
+func branch(b ProgramCounter, C bool, bitmask []bool) (ExitReasonTypes, ProgramCounter) {
+	panic("not implemented")
+}
+
+func djump(a int, jumpTable JumpTable, bitmask []bool) (ExitReasonTypes, ProgramCounter) {
+	panic("not implemented")
+}
