eab42ccf12b759c2e72a27deb8795a59f05f08b9
HanaYukii <islu245777@gmail.com>
2025-03-03 14:45:17 +0800

    upd

--- PolkaVM/host_call.go
+++ PolkaVM/host_call.go
@@ -1,5 +1,4 @@
 package PolkaVM
-
 import "github.com/New-JAMneration/JAM-Protocol/internal/types"
 
 type Psi_H_ReturnType struct {
@@ -76,3 +75,78 @@ type OmegaInput struct {
 	History      *HistoryState             // For storing history state
 	Args         []any                     // Extra parameter for each host-call function
 }
+
+import "fmt"
+
+type Psi_H_ReturnType struct {
+	ExitReason error     // exit reason
+	Counter    uint32    // new instruction counter
+	Gas        Gas       // gas remain
+	Reg        Registers // new registers
+	Ram        Memory    // new memory
+	Addition   any       // addition host-call context
+}
+
+type OmegaReturnType struct {
+	ExitReason error     // exit reason
+	GasRemain  Gas       // gas remain
+	Register   Registers // new registers
+	Ram        Memory    // new memory
+	Addition   any       // addition host-call context
+}
+
+// (A.31) Ψ_H
+func Psi_H(
+	code ProgramCode, // program code
+	counter ProgramCounter, // program counter
+	gas Gas, // gas counter
+	reg Registers, // registers
+	ram Memory, // memory
+	omega Omega, // jump table
+	addition any, // host-call context
+	program StandardProgram,
+) (
+	psi_result Psi_H_ReturnType,
+) {
+	exitreason_prime, counter_prime, gas_prime, reg_prime, memory_prime := SingleStepInvoke(code, counter, gas, reg, ram)
+	fmt.Println(exitreason_prime, counter_prime, gas_prime, reg_prime, memory_prime)
+	reason := exitreason_prime.(*PVMExitReason)
+	if reason.Reason == HALT || reason.Reason == PANIC || reason.Reason == OUT_OF_GAS || reason.Reason == PAGE_FAULT {
+		psi_result.ExitReason = PVMExitTuple(reason.Reason, nil)
+		psi_result.Counter = uint32(counter_prime)
+		psi_result.Gas = gas_prime
+		psi_result.Reg = reg_prime
+		psi_result.Ram = memory_prime
+		psi_result.Addition = addition
+	} else if reason.Reason == HOST_CALL {
+		omega_result := omega(*reason.FaultAddr, gas_prime, reg_prime, ram, addition)
+		omega_reason := omega_result.ExitReason.(*PVMExitReason)
+		if omega_reason.Reason == PAGE_FAULT {
+			psi_result.Counter = uint32(counter_prime)
+			psi_result.Gas = gas_prime
+			psi_result.Reg = reg_prime
+			psi_result.Ram = memory_prime
+			psi_result.ExitReason = PVMExitTuple(PAGE_FAULT, *omega_reason.FaultAddr)
+			psi_result.Addition = addition
+		} else if omega_reason.Reason == CONTINUE {
+			return Psi_H(code, ProgramCounter(skip(int(counter_prime), program.ProgramBlob.Bitmasks)), omega_result.GasRemain, omega_result.Register, omega_result.Ram, omega, omega_result.Addition, program)
+		} else if omega_reason.Reason == PANIC || omega_reason.Reason == OUT_OF_GAS || omega_reason.Reason == HALT {
+			psi_result.ExitReason = omega_result.ExitReason
+			psi_result.Counter = uint32(counter_prime)
+			psi_result.Gas = omega_result.GasRemain
+			psi_result.Reg = omega_result.Register
+			psi_result.Ram = omega_result.Ram
+			psi_result.Addition = omega_result.Addition
+		}
+	}
+	return
+}
+
+// (A.32) Ω⟨X⟩
+type Omega func(
+	uint64, // instruction
+	Gas, // gas counter
+	Registers, // registers
+	Memory, // memory
+	any, // host-call context
+) OmegaReturnType
