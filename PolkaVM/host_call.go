4883cc2c54af7d566490a97e523f38f804237400
HanaYukii <islu245777@gmail.com>
2025-03-03 14:11:14 +0800

    Define general function struct

--- PolkaVM/host_call.go
+++ PolkaVM/host_call.go
@@ -1,21 +1,13 @@
 package PolkaVM
 
+import "github.com/New-JAMneration/JAM-Protocol/internal/types"
+
 type Psi_H_ReturnType struct {
 	Pagefault        bool            // exit reason is page fault (for handling multiple return type)
 	ExitReason       ExitReasonTypes // exit reason
 	Counter          uint64          // new instruction counter
 	Gas              uint64          // gas remain
 	Reg              Registers       // new registers
-	Ram              PageMap         // new memory
-	Addition         any             // addition host-call context
-	PagefaultAddress uint64          // page fault address, only use for page fault
-}
-
-type OmegaReturnType struct {
-	Pagefault        bool            // exit reason is page fault (for handling multiple return type)
-	ExitReason       ExitReasonTypes // exit reason
-	GasRemain        Gas             // gas remain
-	Register         Registers       // new registers
 	Ram              PageMap         // new memory
 	Addition         any             // addition host-call context
 	PagefaultAddress uint64          // page fault address, only use for page fault
@@ -28,7 +20,7 @@ func Psi_H(
 	gas Gas, // gas counter
 	reg Registers, // registers
 	ram PageMap, // memory
-	omega Omega, // jump table
+	omega GeneralFunction, // function
 	addition any, // host-call context
 ) (
 	psi_result Psi_H_ReturnType,
@@ -37,11 +29,66 @@ func Psi_H(
 	return
 }
 
-// (A.32) Ω⟨X⟩
-type Omega func(
-	uint64, // instruction
-	Gas, // gas counter
-	Registers, // registers
-	PageMap, // memory
-	any, // host-call context
-) OmegaReturnType
+// OperationType Enum
+type OperationType int
+
+const (
+	GasOp              OperationType = iota // gas = 0
+	LookupOp                                // lookup = 1
+	ReadOp                                  // read = 2
+	WriteOp                                 // write = 3
+	InfoOp                                  // info = 4
+	BlessOp                                 // bless = 5
+	AssignOp                                // assign = 6
+	DesignateOp                             // designate = 7
+	CheckpointOp                            // checkpoint = 8
+	NewOp                                   // new = 9
+	UpgradeOp                               // upgrade = 10
+	TransferOp                              // transfer = 11
+	EjectOp                                 // eject = 12
+	QueryOp                                 // query = 13
+	SolicitOp                               // solicit = 14
+	ForgetOp                                // forget = 15
+	YieldOp                                 // yield = 16
+	HistoricalLookupOp                      // historical_lookup = 17
+	FetchOp                                 // fetch = 18
+	ExportOp                                // export = 19
+	MachineOp                               // machine = 20
+	PeekOp                                  // peek = 21
+	PokeOp                                  // poke = 22
+	ZeroOp                                  // zero = 23
+	VoidOp                                  // void = 24
+	InvokeOp                                // invoke = 25
+	ExpungeOp                               // expunge = 26
+)
+
+type HistoryState struct {
+	PreviousGas       Gas
+	PreviousRegisters Registers
+	PreviousMemory    PageMap
+	PreviousAccount   types.ServiceAccountState
+}
+
+type OmegaOutput struct {
+	ExitReason      error                     // Exit reason
+	NewGas          Gas                       // New Gas
+	NewRegisters    Registers                 // New Register
+	NewMemory       PageMap                   // New Memory
+	NewAccountState types.ServiceAccountState // New State
+	Counter         int64                     // For calculate run count
+	History         *HistoryState             // For roll back
+	Addition        []any                     // addition host-call context
+}
+
+type GeneralFunction func(OmegaInput) OmegaOutput
+
+type OmegaInput struct {
+	Instruction  uint64                    // opcode
+	Operation    OperationType             // operation type
+	Gas          Gas                       // gas counter
+	Registers    Registers                 // PVM registers
+	Memory       PageMap                   // memory
+	AccountState types.ServiceAccountState // State
+	History      *HistoryState             // For storing history state
+	Args         []any                     // Extra parameter for each host-call function
+}
