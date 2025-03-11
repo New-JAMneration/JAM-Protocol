146568be01829e298b417bd538187208f397440b
yu2C <c2001413@gmail.com>
2025-03-11 23:51:21 +0800

    refactor: update const

--- internal/types/const.go
+++ internal/types/const.go
@@ -103,19 +103,21 @@ const (
 )
 
 const (
-	MaximumWorkItems                 = 16 // I (graypaper 0.6.3)
-	MaximumDependencyItems           = 8  // J
-	WorkReportTimeout                = 5  // U
-	WorkReportOutputBlobsMaximumSize = 48 * 1024
-	MaxLookupAge                     = 14400 // L
+	MaximumWorkItems                 = 16        // I (graypaper 0.6.3)
+	MaximumDependencyItems           = 8         // J
+	WorkReportTimeout                = 5         // U
+	WorkReportOutputBlobsMaximumSize = 48 * 1024 // W_R
+	MaxLookupAge                     = 14400     // L
 )
 
 // work package constants
 const (
-	MaxTotalSize     = 12 * 1024 * 1024 // W_B=12 MB (14.6)
-	MaxRefineGas     = 5_000_000_000    // G_R v0.6.3
-	MaxAccumulateGas = 10_000_000       // G_A v0.6.3
-	MaxSegments      = 3072             // import/export segment total limit 3072 (14.4). graypaper 0.6.3
-	SegmentSize      = 4104             // size of segment
-	MaxExtrinsics    = 128              // T (14.4). graypaper 0.6.3
+	MaxTotalSize       = 12 * 1024 * 1024                 // W_B = 12 MB (14.6)
+	MaxRefineGas       = 5_000_000_000                    // G_R v0.6.3
+	MaxAccumulateGas   = 10_000_000                       // G_A v0.6.3
+	MaxSegments        = 3072                             // W_M: import/export segment total limit (14.4). graypaper 0.6.3
+	ECPiecesPerSegment = 6                                // W_P: The number of erasure-coded pieces in a segment
+	ECBasicSize        = 684                              // W_E: The basic size of erasure-coded pieces in octets
+	SegmentSize        = ECPiecesPerSegment * ECBasicSize // W_G = 4104: The size of a segment in octets
+	MaxExtrinsics      = 128                              // T (14.4). graypaper 0.6.3
 )
