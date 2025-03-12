e4ab5e04d86bd88bb0e2c292e97a8ac09610caa9
TwEricShen <erick7shen@gmail.com>
2025-03-12 19:31:25 +0800

    resolve commented problems

--- PolkaVM/branch.go
+++ PolkaVM/branch.go
@@ -15,11 +15,12 @@ func djump(pc ProgramCounter, a uint32, jumpTable JumpTable, bitmask Bitmask) (E
 	switch {
 	case a == 0xffff0000:
 		return HALT, pc
-	case a == 0 || a > jumpTable.Length*ZA || a%jumpTable.Length != 0:
+		// jumpTable Size : |j|  , jumpTable Length E_1(z)
+	case a == 0 || a > jumpTable.Size*ZA || a%ZA != 0:
 		return PANIC, pc
 	}
 
-	index := a/ZA - 1
+	index := a/ZA - 1 // GP
 
 	dest, _, err := ReadUintFixed(jumpTable.Data[index:], int(jumpTable.Size))
 	if err != nil {
