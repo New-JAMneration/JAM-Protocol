5103fc37d8298a81be4fdf16550cd6a8421754ff
terryhung <terryhung1228@gmail.com>
2025-03-14 02:06:03 +0800

    feat: add the quic

--- go.mod
+++ go.mod
@@ -17,21 +17,30 @@ require (
 require (
 	github.com/alicebob/miniredis/v2 v2.34.0
 	github.com/go-redis/redis v6.15.9+incompatible
-	github.com/stretchr/testify v1.5.1
+	github.com/quic-go/quic-go v0.50.0
+	github.com/stretchr/testify v1.9.0
 	github.com/test-go/testify v1.1.4
 )
 
 require github.com/google/go-cmp v0.7.0
 
-require github.com/stretchr/objx v0.1.0 // indirect
+require github.com/stretchr/objx v0.5.2 // indirect
 
 require (
 	github.com/alicebob/gopher-json v0.0.0-20230218143504-906a9b012302 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/google/pprof v0.0.0-20241210010833-40e02aabc2ad // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/ginkgo/v2 v2.22.1 // indirect
 	github.com/onsi/gomega v1.36.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/yuin/gopher-lua v1.1.1 // indirect
+	go.uber.org/mock v0.5.0 // indirect
 	golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	golang.org/x/mod v0.23.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/tools v0.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/networking/handler/ce/mock_stream.go
@@ -0,0 +1,49 @@
+package ce
+
+import (
+	"bytes"
+	"context"
+	"time"
+
+	quicgo "github.com/quic-go/quic-go"
+)
+
+// --- mockStream implements a minimal in-memory quic.Stream.
+type mockStream struct {
+	r      *bytes.Reader // used for reading incoming request bytes
+	w      *bytes.Buffer // used for capturing written response bytes
+	closed bool
+}
+
+func newMockStream(initialData []byte) *mockStream {
+	return &mockStream{
+		r: bytes.NewReader(initialData),
+		w: new(bytes.Buffer),
+	}
+}
+
+func (m *mockStream) Read(p []byte) (int, error) {
+	return m.r.Read(p)
+}
+
+func (m *mockStream) Write(p []byte) (int, error) {
+	return m.w.Write(p)
+}
+
+func (m *mockStream) Close() error {
+	m.closed = true
+	return nil
+}
+
+func (m *mockStream) Context() context.Context {
+	return context.Background()
+}
+
+func (m *mockStream) SetDeadline(t time.Time) error      { return nil }
+func (m *mockStream) SetReadDeadline(t time.Time) error  { return nil }
+func (m *mockStream) SetWriteDeadline(t time.Time) error { return nil }
+
+// Added to satisfy quic.Stream interface.
+func (m *mockStream) CancelRead(quicgo.StreamErrorCode)  {}
+func (m *mockStream) CancelWrite(quicgo.StreamErrorCode) {}
+func (m *mockStream) StreamID() quicgo.StreamID          { return 0 }
