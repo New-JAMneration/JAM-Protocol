f06b90c3168c6bcaa0d0fcb191a235c94d109a60
Yu Chun Tsao <tsao84672776@gmail.com>
2025-03-16 21:10:25 +0800

    feat: Added function to validate TLS certificate
    - The certificate uses Ed25519 as the signature algorithm.
    - The SAN is exactly 53 characters, starting with "e" followed by a base32 encoded string (using the specified alphabet).
    - The certificate’s public key matches the information encoded in the SAN.

---
+++ internal/networking/cert/validator_test.go
@@ -0,0 +1,150 @@
+package network
+
+import (
+	"crypto/ed25519"
+	"testing"
+)
+
+func TestValidateTlsSignatureAlgorithm(t *testing.T) {
+	type PkSk struct {
+		sk ed25519.PrivateKey
+		pk ed25519.PublicKey
+	}
+
+	pksk := PkSk{
+		pk: strToHex("0x3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+		sk: strToHex("0x00000000000000000000000000000000000000000000000000000000000000003b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+	}
+
+	tlsCert, err := GenerateSelfSignedCertificate(pksk.sk, pksk.pk)
+	if err != nil {
+		t.Errorf("GenerateSelfSignedCertificate() error = %v", err)
+		return
+	}
+
+	err = ValidateTlsSignatureAlgorithm(tlsCert)
+	if err != nil {
+		t.Errorf("ValidateTlsSignatureAlgorithm() error = %v", err)
+		return
+	}
+}
+
+func TestValidateX509SignatureAlgorithm(t *testing.T) {
+	type PkSk struct {
+		sk ed25519.PrivateKey
+		pk ed25519.PublicKey
+	}
+
+	pksk := PkSk{
+		pk: strToHex("0x3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+		sk: strToHex("0x00000000000000000000000000000000000000000000000000000000000000003b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+	}
+
+	tlsCert, err := GenerateSelfSignedCertificate(pksk.sk, pksk.pk)
+	if err != nil {
+		t.Errorf("GenerateSelfSignedCertificate() error = %v", err)
+		return
+	}
+
+	err = ValidateX509SignatureAlgorithm(*tlsCert.Leaf)
+	if err != nil {
+		t.Errorf("ValidateX509SignatureAlgorithm() error = %v", err)
+		return
+	}
+}
+
+func TestValidateTlsDNSNames(t *testing.T) {
+	type PkSk struct {
+		sk ed25519.PrivateKey
+		pk ed25519.PublicKey
+	}
+
+	pksk := PkSk{
+		pk: strToHex("0x3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+		sk: strToHex("0x00000000000000000000000000000000000000000000000000000000000000003b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+	}
+
+	tlsCert, err := GenerateSelfSignedCertificate(pksk.sk, pksk.pk)
+	if err != nil {
+		t.Errorf("GenerateSelfSignedCertificate() error = %v", err)
+		return
+	}
+
+	err = ValidateTlsDNSNames(tlsCert)
+	if err != nil {
+		t.Errorf("ValidateTlsDNSNames() error = %v", err)
+		return
+	}
+}
+
+func TestValidateX509DNSNames(t *testing.T) {
+	type PkSk struct {
+		sk ed25519.PrivateKey
+		pk ed25519.PublicKey
+	}
+
+	pksk := PkSk{
+		pk: strToHex("0x3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+		sk: strToHex("0x00000000000000000000000000000000000000000000000000000000000000003b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+	}
+
+	tlsCert, err := GenerateSelfSignedCertificate(pksk.sk, pksk.pk)
+	if err != nil {
+		t.Errorf("GenerateSelfSignedCertificate() error = %v", err)
+		return
+	}
+
+	err = ValidateX509DNSNames(*tlsCert.Leaf)
+	if err != nil {
+		t.Errorf("ValidateX509DNSNames() error = %v", err)
+		return
+	}
+}
+
+func TestValidateX509PubKeyMatchesSAN(t *testing.T) {
+	type PkSk struct {
+		sk ed25519.PrivateKey
+		pk ed25519.PublicKey
+	}
+
+	pksk := PkSk{
+		pk: strToHex("0x3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+		sk: strToHex("0x00000000000000000000000000000000000000000000000000000000000000003b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+	}
+
+	tlsCert, err := GenerateSelfSignedCertificate(pksk.sk, pksk.pk)
+	if err != nil {
+		t.Errorf("GenerateSelfSignedCertificate() error = %v", err)
+		return
+	}
+
+	err = ValidateX509PubKeyMatchesSAN(*tlsCert.Leaf)
+	if err != nil {
+		t.Errorf("ValidateX509PubKeyMatchesSAN() error = %v", err)
+		return
+	}
+}
+
+func TestValidateTlsPubKeyMatchesSAN(t *testing.T) {
+	type PkSk struct {
+		sk ed25519.PrivateKey
+		pk ed25519.PublicKey
+	}
+
+	pksk := PkSk{
+		pk: strToHex("0x3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+		sk: strToHex("0x00000000000000000000000000000000000000000000000000000000000000003b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29"),
+	}
+
+	tlsCert, err := GenerateSelfSignedCertificate(pksk.sk, pksk.pk)
+	if err != nil {
+		t.Errorf("GenerateSelfSignedCertificate() error = %v", err)
+		return
+	}
+
+	err = ValidateTlsPubKeyMatchesSAN(tlsCert)
+	if err != nil {
+		t.Errorf("ValidateTlsPubKeyMatchesSAN() error = %v", err)
+		return
+	}
+}
