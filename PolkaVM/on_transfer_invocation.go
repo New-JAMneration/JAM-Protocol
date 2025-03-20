09c686b15d464b8a614f8b457338e6a4bfc8eab7
TwEricShen <erick7shen@gmail.com>
2025-03-20 20:21:42 +0800

    temporary commit

--- PolkaVM/on_transfer_invocation.go
+++ PolkaVM/on_transfer_invocation.go
@@ -1,14 +1,57 @@
 package PolkaVM
 
-import "github.com/New-JAMneration/JAM-Protocol/internal/types"
+import (
+	"log"
+
+	"github.com/New-JAMneration/JAM-Protocol/internal/types"
+)
 
 type OnTransferInput struct {
-	ServiceAccounts  types.ServiceAccountState
-	Timeslot         types.TimeSlot
-	ServiceID        types.ServiceId
-	DeferredTransfer types.DeferredTransfer
+	ServiceAccounts   types.ServiceAccountState
+	Timeslot          types.TimeSlot
+	ServiceID         types.ServiceId
+	DeferredTransfers []types.DeferredTransfer
 }
 
-func OnTransferInvoke(OnTransferInput) types.ServiceAccount {
-	return types.ServiceAccount{}
+// Psi_T
+func OnTransferInvoke(input OnTransferInput) (types.ServiceAccount, types.Gas) {
+	emptyArray := types.OpaqueHash{}
+
+	if account, accountExists := input.ServiceAccounts[input.ServiceID]; accountExists {
+		if account.ServiceInfo.CodeHash == emptyArray || len(input.DeferredTransfers) == 0 {
+			return input.ServiceAccounts[input.ServiceID], 0
+		}
+		// arguments
+		serialized := []byte{}
+		gasLimits := Gas(0)
+		balances := types.U64(0)
+		for _, deferredTransfer := range input.DeferredTransfers {
+			gasLimits += deferredTransfer.GasLimit
+			balances += deferredTransfer.Balance
+		}
+
+		encoder := types.Encoder{}
+		s := input.ServiceAccounts[input.ServiceID]
+		s.ServiceInfo.Balance += balances
+		// F := hostCallFunctions()
+
+		// timeslot(t) bytes
+		timeslotSer, err := encoder.Encode(input.Timeslot)
+		if err != nil {
+			log.Println("OnTransferInvoke encode TimeSlot error")
+		}
+		// serviceID(s) bytes
+		serviceIDSer, err := encoder.Encode(input.ServiceID)
+
+		serialized = append(timeslotSer, serviceIDSer...)
+
+		// !warning: should input service count blob, not codehash, how to get code blob ?
+		result := Psi_M(account.ServiceInfo.CodeHash[:], 10, gasLimits, serialized, F, nil, s)
+
+		// Psi_M
+	} else {
+		log.Println("OnTransferInvoke account not exists")
+	}
+
+	return types.ServiceAccount{}, 0
 }
