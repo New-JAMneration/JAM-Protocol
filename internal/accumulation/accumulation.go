13e679e821727124769295d18a6dc9f92ab549f4
Yu Chun Tsao <tsao84672776@gmail.com>
2025-03-22 00:32:12 +0800

    feat: Add accumulation function and types

---
+++ internal/accumulation/accumulation.go
@@ -0,0 +1,25 @@
+package accumulation
+
+// (12.16)
+// outer accumulation function
+func OuterAccumulation(input OuterAccumulationInput) (OuterAccumulationOutput, error) {
+	var err error
+
+	return OuterAccumulationOutput{}, err
+}
+
+// (12.17)
+// parallelized accumulation function
+func ParallelizedAccumulation(input ParallelizedAccumulationInput) (ParallelizedAccumulationOutput, error) {
+	var err error
+
+	return ParallelizedAccumulationOutput{}, err
+}
+
+// (12.19)
+// single-service accumulation function
+func SingleServiceAccumulation(input SingleServiceAccumulationInput) (SingleServiceAccumulationOutput, error) {
+	var err error
+
+	return SingleServiceAccumulationOutput{}, err
+}
