8daff1f008f1b318f6fce5f26411a388124b1c3a
Yu Chun Tsao <tsao84672776@gmail.com>
2025-03-22 15:52:48 +0800

    doc: Add doc for encoder and decoder

---
+++ internal/types/decode_test.go
@@ -0,0 +1,91 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func GetBytesFromFile(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %v", err)
+	}
+
+	return data, nil
+}
+
+func GetBlockFromJson(filename string) (*Block, error) {
+	data, err := GetBytesFromFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get bytes from file: %v", err)
+	}
+
+	// Unmarshal the JSON data
+	var block Block
+	err = json.Unmarshal(data, &block)
+	if err != nil {
+		return nil, err
+	}
+
+	return &block, nil
+}
+
+func TestDecodeBlock(t *testing.T) {
+	blockBinFile := "../../pkg/test_data/jamtestnet/chainspecs/blocks/genesis-tiny.bin"
+
+	// Get the bytes from the file
+	data, err := GetBytesFromFile(blockBinFile)
+	if err != nil {
+		t.Errorf("Error getting bytes from file: %v", err)
+	}
+
+	// Create a new block (pointer)
+	block := &Block{}
+
+	// Decode the block from the binary file
+	decoder := NewDecoder()
+	err = decoder.Decode(data, block)
+	if err != nil {
+		t.Errorf("Error decoding block: %v", err)
+	}
+
+	// Check the decode block is same as expected block
+	// Read json file and unmarshal it to get the expected block
+	blockJsonFile := "../../pkg/test_data/jamtestnet/chainspecs/blocks/genesis-tiny.json"
+	expectedBlock, err := GetBlockFromJson(blockJsonFile)
+	if err != nil {
+		t.Errorf("Error getting block from json file: %v", err)
+	}
+
+	// Compare the two blocks
+	if !reflect.DeepEqual(block, expectedBlock) {
+		t.Errorf("Blocks do not match")
+	}
+}
+
+func TestDecodeU64(t *testing.T) {
+	data := []byte{0, 228, 11, 84, 2, 0, 0, 0}
+
+	decoder := NewDecoder()
+	u64 := U64(0)
+	err := decoder.Decode(data, &u64)
+	if err != nil {
+		t.Errorf("Error decoding U64: %v", err)
+	}
+
+	expected := U64(10000000000)
+
+	if u64 != expected {
+		t.Errorf("Decoded U64 does not match expected")
+	}
+}
