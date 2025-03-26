05d8f87617f7c0bbe6e3282e883eeb2cbb0af51c
Yu Chun Tsao <tsao84672776@gmail.com>
2025-03-26 16:05:45 +0800

    test: read file from jam-test-vectors

--- internal/types/decode_test.go
+++ internal/types/decode_test.go
@@ -1,4 +1,4 @@
-package types
+package types_test
 
 import (
 	"encoding/json"
@@ -7,6 +7,9 @@ import (
 	"os"
 	"reflect"
 	"testing"
+
+	"github.com/New-JAMneration/JAM-Protocol/internal/types"
+	jamtests_accmuluate "github.com/New-JAMneration/JAM-Protocol/jamtests/accumulate"
 )
 
 func GetBytesFromFile(filename string) ([]byte, error) {
@@ -24,14 +27,14 @@ func GetBytesFromFile(filename string) ([]byte, error) {
 	return data, nil
 }
 
-func GetBlockFromJson(filename string) (*Block, error) {
+func GetBlockFromJson(filename string) (*types.Block, error) {
 	data, err := GetBytesFromFile(filename)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get bytes from file: %v", err)
 	}
 
 	// Unmarshal the JSON data
-	var block Block
+	var block types.Block
 	err = json.Unmarshal(data, &block)
 	if err != nil {
 		return nil, err
@@ -50,10 +53,10 @@ func TestDecodeBlock(t *testing.T) {
 	}
 
 	// Create a new block (pointer)
-	block := &Block{}
+	block := &types.Block{}
 
 	// Decode the block from the binary file
-	decoder := NewDecoder()
+	decoder := types.NewDecoder()
 	err = decoder.Decode(data, block)
 	if err != nil {
 		t.Errorf("Error decoding block: %v", err)
@@ -76,14 +79,14 @@ func TestDecodeBlock(t *testing.T) {
 func TestDecodeU64(t *testing.T) {
 	data := []byte{0, 228, 11, 84, 2, 0, 0, 0}
 
-	decoder := NewDecoder()
-	u64 := U64(0)
+	decoder := types.NewDecoder()
+	u64 := types.U64(0)
 	err := decoder.Decode(data, &u64)
 	if err != nil {
 		t.Errorf("Error decoding U64: %v", err)
 	}
 
-	expected := U64(10000000000)
+	expected := types.U64(10000000000)
 
 	if u64 != expected {
 		t.Errorf("Decoded U64 does not match expected")
@@ -93,16 +96,35 @@ func TestDecodeU64(t *testing.T) {
 func TestDecodeServiceId(t *testing.T) {
 	data := []byte{100, 0, 0, 0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32}
 
-	decoder := NewDecoder()
-	serviceId := ServiceId(0)
+	decoder := types.NewDecoder()
+	serviceId := types.ServiceId(0)
 	err := decoder.Decode(data, &serviceId)
 	if err != nil {
 		t.Errorf("Error decoding ServiceId: %v", err)
 	}
 
-	expected := ServiceId(100)
+	expected := types.ServiceId(100)
 
 	if serviceId != expected {
 		t.Errorf("Decoded ServiceId does not match expected")
 	}
+}
+
+func TestDecodeJamTestVectorsAccumulateFile(t *testing.T) {
+	file := "../../pkg/test_data/jam-test-vectors/accumulate/tiny/accumulate_ready_queued_reports-1.bin"
+
+	data, err := GetBytesFromFile(file)
+	if err != nil {
+		t.Errorf("Error getting bytes from file: %v", err)
+	}
+
+	decoder := types.NewDecoder()
+	accumulateTestCase := &jamtests_accmuluate.AccumulateTestCase{}
+	err = decoder.Decode(data, accumulateTestCase)
+	if err != nil {
+		t.Errorf("Error decoding AccumulateTestCase: %v", err)
+	}
+
+	// You can access the fields of the AccumulateTestCase struct
+	// e.g. accumulateTestCase.Input.Slot
 }
