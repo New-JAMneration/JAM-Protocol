4cbfb336d12e8be0034e678f1b9678b5269105de
TwEricShen <erick7shen@gmail.com>
2025-03-28 07:15:55 +0800

    resolve bugs

--- PolkaVM/on_transfer_invocation.go
+++ PolkaVM/on_transfer_invocation.go
@@ -19,68 +19,58 @@ func OnTransferInvoke(input OnTransferInput) (types.ServiceAccount, types.Gas) {
 	result := Psi_M_ReturnType{}
 	account := types.ServiceAccount{}
 
-	if account, accountExists := input.ServiceAccounts[input.ServiceID]; accountExists {
-		if account.ServiceInfo.CodeHash == emptyArray || len(input.DeferredTransfers) == 0 {
-			return input.ServiceAccounts[input.ServiceID], 0
-		}
-
-		encoder := types.Encoder{}
-		// Psi_M arguments
-		gasLimits := types.Gas(0)
-		balances := types.U64(0)
-
-		F := Omegas{}
-
-		s := input.ServiceAccounts[input.ServiceID]
-		s.ServiceInfo.Balance += balances
-
-		// timeslot(t) bytes
-		timeslotSer, err := encoder.Encode(input.Timeslot)
-		if err != nil {
-			log.Fatalf("OnTransferInvoke encode TimeSlot error")
-		}
-		deferredTransferSer, err := encoder.Encode(input.DeferredTransfers)
-		if err != nil {
-			log.Fatalf("OnTransferInvoke encode DeferredTransfer error")
-		}
-
-		// serviceID(s) bytes
-		serviceIDSer, err := encoder.Encode(input.ServiceID)
-		if err != nil {
-			log.Fatalf("OnTransferInvoke encode ServiceID error")
-		}
-
-		var serialized []byte
-		serialized = append(timeslotSer, serviceIDSer...)
-		serialized = append(serialized, deferredTransferSer...)
-
-		for _, deferredTransfer := range input.DeferredTransfers {
-			gasLimits += deferredTransfer.GasLimit
-			balances += deferredTransfer.Balance
-		}
-
-		// omegas
-		F[LookupOp] = hostCallFunctions[0]
-		F[ReadOp] = hostCallFunctions[ReadOp]
-		F[WriteOp] = hostCallFunctions[WriteOp]
-		F[GasOp] = hostCallFunctions[GasOp]
-		F[InfoOp] = hostCallFunctions[InfoOp]
-		F[26] = onTransferHostCallException
-		// addition, on-transfer only uses GeneralArgs
-		addition := HostCallArgs{
-			GeneralArgs: GeneralArgs{
-				ServiceAccount:      input.ServiceAccounts[input.ServiceID],
-				ServiceId:           input.ServiceID,
-				ServiceAccountState: input.ServiceAccounts,
-			},
-		}
-
-		result := Psi_M(account.ServiceInfo.CodeHash[:], 10, Gas(gasLimits), serialized, F, addition)
-		account = result.Addition.ServiceAccount
-
-	} else {
+	account, accountExists := input.ServiceAccounts[input.ServiceID]
+	if !accountExists {
 		log.Fatalf("OnTransferInvoke serviceAccount : %d not exists", input.ServiceID)
 	}
+
+	if account.ServiceInfo.CodeHash == emptyArray || len(input.DeferredTransfers) == 0 {
+		return input.ServiceAccounts[input.ServiceID], 0
+	}
+
+	encoder := types.NewEncoder()
+	// Psi_M arguments
+	gasLimits := types.Gas(0)
+	balances := types.U64(0)
+
+	F := Omegas{}
+
+	s := input.ServiceAccounts[input.ServiceID]
+	s.ServiceInfo.Balance += balances
+
+	// timeslot(t) bytes
+	timeslotSer, _ := encoder.Encode(input.Timeslot)
+	deferredTransferSer, _ := encoder.Encode(input.DeferredTransfers)
+	// serviceID(s) bytes
+	serviceIDSer, _ := encoder.Encode(input.ServiceID)
+
+	var serialized []byte
+	serialized = append(timeslotSer, serviceIDSer...)
+	serialized = append(serialized, deferredTransferSer...)
+
+	for _, deferredTransfer := range input.DeferredTransfers {
+		gasLimits += deferredTransfer.GasLimit
+		balances += deferredTransfer.Balance
+	}
+
+	// omegas
+	F[LookupOp] = hostCallFunctions[0]
+	F[ReadOp] = hostCallFunctions[ReadOp]
+	F[WriteOp] = hostCallFunctions[WriteOp]
+	F[GasOp] = hostCallFunctions[GasOp]
+	F[InfoOp] = hostCallFunctions[InfoOp]
+	F[27] = onTransferHostCallException
+	// addition, on-transfer only uses GeneralArgs
+	addition := HostCallArgs{
+		GeneralArgs: GeneralArgs{
+			ServiceAccount:      input.ServiceAccounts[input.ServiceID],
+			ServiceId:           input.ServiceID,
+			ServiceAccountState: input.ServiceAccounts,
+		},
+	}
+
+	result = Psi_M(account.ServiceInfo.CodeHash[:], 10, Gas(gasLimits), serialized, F, addition)
+	account = result.Addition.ServiceAccount
 
 	return account, types.Gas(result.Gas)
 }
