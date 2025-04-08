372a2836496ab38c47a6f8d918da5dd972416f46
jory <starcorn2020@gmail.com>
2025-04-08 21:40:06 +0800

    delete comment

--- PolkaVM/is_authorized_invocation.go
+++ PolkaVM/is_authorized_invocation.go
@@ -9,40 +9,24 @@ func Psi_I(p types.WorkPackage, c types.CoreIndex, code types.OpaqueHash) Psi_I_
 	serialized := []byte{}
 	encoder := types.NewEncoder()
 
-	// todo 改寫法
-	encoded_p, err := encoder.Encode(&p)
+	encoded, err := encoder.Encode(&p)
 	if err != nil {
 		panic(err)
 	}
+	serialized = append(serialized, encoded...)
 
-	encoded_c, err := encoder.Encode(&c)
+	encoded, err = encoder.Encode(&c)
 	if err != nil {
 		panic(err)
 	}
-
-	serialized = append(serialized, encoded_p...)
-	serialized = append(serialized, encoded_c...)
+	serialized = append(serialized, encoded...)
 
 	F := Omegas{}
-	F[GasOp] = hostCallFunctions[GasOp]
+	F[GasOp] = HostCallFunctions[GasOp]
 	F[27] = accumulateInvocationHostCallException
 
-	// 准备HostCallArgs
-	// 參數全空
-	addition := HostCallArgs{
-		// todo: 看灰皮書
-		GeneralArgs: GeneralArgs{
-			ServiceAccount:      types.ServiceAccount{},
-			ServiceId:           types.ServiceId(0),
-			ServiceAccountState: types.ServiceAccountState{},
-		},
-		AccumulateArgs: AccumulateArgs{
-			ResultContextX: ResultContext{},
-			ResultContextY: ResultContext{},
-		},
-	}
+	addition := HostCallArgs{}
 
-	// todo: 找 gas
 	resultM := Psi_M(code[:], 0, Gas(types.IsAuthorizedGas), Argument(serialized), F, addition)
 	if resultM.ReasonOrBytes == PANIC {
 		return Psi_I_ReturnType{
@@ -57,7 +41,6 @@ func Psi_I(p types.WorkPackage, c types.CoreIndex, code types.OpaqueHash) Psi_I_
 		}
 	}
 
-	// 斷言 resultM.ReasonOrBytes 是 []byte
 	workOutput, ok := resultM.ReasonOrBytes.([]byte)
 	if !ok {
 		return Psi_I_ReturnType{
@@ -75,7 +58,6 @@ func Psi_I(p types.WorkPackage, c types.CoreIndex, code types.OpaqueHash) Psi_I_
 
 type Psi_I_ReturnType struct {
 	WorkExecResult types.WorkExecResultType
-	// todo: R 的返回值
-	WorkOutput []byte
-	Gas        types.Gas
+	WorkOutput     []byte
+	Gas            types.Gas
 }
