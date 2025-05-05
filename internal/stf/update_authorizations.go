e3c705713b853ddc0bf65dd231387a8c09ab136e
yu2C <c2001413@gmail.com>
2025-05-06 00:09:18 +0800

    feat: STF authorizations only tiny

--- internal/stf/update_authorizations.go
+++ internal/stf/update_authorizations.go
@@ -1,5 +1,12 @@
 package stf
 
+import "github.com/New-JAMneration/JAM-Protocol/internal/authorization"
+
 func UpdateAuthorizations() error {
+	// === Run Authorization ===
+	err := authorization.Authorization()
+	if err != nil {
+		return err
+	}
 	return nil
 }
