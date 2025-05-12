a4f2d1be9af894b37ec85663216e7ab7bf0100a0
Yu Chun Tsao <tsao84672776@gmail.com>
2025-05-12 17:55:55 +0800

    fix: Update state-key constructor

--- internal/utilities/merklization/state_key_constructor.go
+++ internal/utilities/merklization/state_key_constructor.go
@@ -2,12 +2,13 @@ package merklization
 
 import (
 	"github.com/New-JAMneration/JAM-Protocol/internal/types"
-	"github.com/New-JAMneration/JAM-Protocol/internal/utilities"
 )
+
+type StateKey [31]byte
 
 // StateKeyConstruct is an interface
 type StateKeyConstruct interface {
-	StateKeyConstruct() (output types.OpaqueHash)
+	StateKeyConstruct() (output StateKey)
 }
 
 // StateWrapper is a wrapper for U8
@@ -15,45 +16,60 @@ type StateWrapper struct {
 	StateIndex types.U8
 }
 
-// StateServiceWrapper is a wrapper for U8 and U32
+// StateServiceWrapper is a wrapper for U8 and ServiceId(U32)
 type StateServiceWrapper struct {
 	StateIndex   types.U8
-	ServiceIndex types.U32
+	ServiceIndex types.ServiceId
 }
 
-// ServiceWrapper is a wrapper for U32 and OpaqueHash
+// ServiceWrapper is a wrapper for ServiceId(U32) and a byte array of length 27
 type ServiceWrapper struct {
-	ServiceIndex types.U32
-	Hash         types.OpaqueHash
+	ServiceIndex types.ServiceId
+	h            [27]byte
+}
+
+func encodeServiceId(serviceId types.ServiceId) []byte {
+	encoder := types.NewEncoder()
+	encoded, _ := encoder.EncodeUintWithLength(uint64(serviceId), 4)
+
+	return encoded
 }
 
 // D.1 State-Key-Construction
-
-// StateKeyConstruct returns a OpaqueHash
-func (s StateWrapper) StateKeyConstruct() (output types.OpaqueHash) {
+func (s StateWrapper) StateKeyConstruct() (output StateKey) {
+	// [i, 0, 0,...]
 	output[0] = byte(s.StateIndex)
 	return output
 }
 
-// StateKeyConstruct returns a OpaqueHash
-func (w StateServiceWrapper) StateKeyConstruct() (output types.OpaqueHash) {
+func (w StateServiceWrapper) StateKeyConstruct() (output StateKey) {
+	// [i, n_0, 0, n_1, 0, n2, 0, n3, 0, 0,...] where n = encode_4(service_id)
 	output[0] = byte(w.StateIndex)
-	Serialized := utilities.SerializeFixedLength(w.ServiceIndex, 4)
-	for i := 0; i < 4; i++ {
-		output[2*i+1] = Serialized[i]
+
+	// Encode the service index
+	n := encodeServiceId(w.ServiceIndex)
+
+	for i := 0; i <= 3; i++ {
+		output[2*i+1] = n[i]
 	}
 	return output
 }
 
 // StateKeyConstruct returns a OpaqueHash
-func (w ServiceWrapper) StateKeyConstruct() (output types.OpaqueHash) {
-	Serialized := utilities.SerializeFixedLength(w.ServiceIndex, 4)
-	for i := 0; i < 4; i++ {
-		output[2*i] = Serialized[i]
-		output[2*i+1] = w.Hash[i]
+func (w ServiceWrapper) StateKeyConstruct() (output StateKey) {
+	// [n_0, h_0, n_1, h_1, n_2, h_2, n_3, h_3, h_4, h_5,...,h_26] where n = encode_4(service_id)
+
+	// Encode the service index
+	n := encodeServiceId(w.ServiceIndex)
+
+	for i := 0; i <= 3; i++ {
+		output[2*i] = n[i]
+		output[2*i+1] = w.h[i]
 	}
-	for i := 4; i < 28; i++ {
-		output[i+4] = w.Hash[i]
+
+	for i := 4; i <= 26; i++ {
+		output[i+4] = w.h[i]
 	}
+
 	return output
 }
