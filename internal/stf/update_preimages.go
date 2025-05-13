79a64a96a8d00835eee282fb76ff7614eb560491
yu2C <c2001413@gmail.com>
2025-05-13 15:28:13 +0800

    feat: STF preimages

--- internal/stf/update_preimages.go
+++ internal/stf/update_preimages.go
@@ -1,5 +1,11 @@
 package stf
 
+import "github.com/New-JAMneration/JAM-Protocol/internal/accumulation"
+
 func UpdatePreimages() error {
+	err := accumulation.ProcessPreimageExtrinsics()
+	if err != nil {
+		return err
+	}
 	return nil
 }
