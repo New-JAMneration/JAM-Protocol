2614d345600bc6b8fadbd6a46d4be95f8a880a53
Yu Chun Tsao <tsao84672776@gmail.com>
2025-05-13 23:59:00 +0800

    fix: Move common types to internal/types

--- jamtests/trace/decode.go
+++ jamtests/trace/decode.go
@@ -2,37 +2,6 @@ package jamtests
 
 import "github.com/New-JAMneration/JAM-Protocol/internal/types"
 
-// Decode StateKeyVals
-func (s *StateKeyVals) Decode(d *types.Decoder) error {
-	var err error
-
-	// Decode the length of the array
-	length, err := d.DecodeLength()
-	if err != nil {
-		return err
-	}
-
-	if length == 0 {
-		return nil
-	}
-
-	// Allocate space for the array
-	*s = make(StateKeyVals, length)
-
-	// Decode each element in the array
-	for i := uint64(0); i < length; i++ {
-		if err = (*s)[i].Key.Decode(d); err != nil {
-			return err
-		}
-
-		if err = (*s)[i].Value.Decode(d); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 // Decode TraceState
 func (s *TraceState) Decode(d *types.Decoder) error {
 	var err error
