439075dc0c3a2db2aa12c92fa3b87f3b0e6a6140
yu2C <c2001413@gmail.com>
2025-05-15 13:50:40 +0800

    refactor: safrole integrated by epoch/slot

--- internal/safrole/safrole.go
+++ internal/safrole/safrole.go
@@ -2,11 +2,12 @@ package safrole
 
 import (
 	"fmt"
+	"log"
 	"slices"
-	"time"
 
 	"github.com/New-JAMneration/JAM-Protocol/internal/store"
 	"github.com/New-JAMneration/JAM-Protocol/internal/types"
+	SafroleErrorCode "github.com/New-JAMneration/JAM-Protocol/internal/types/error_codes/safrole"
 	vrf "github.com/New-JAMneration/JAM-Protocol/pkg/Rust-VRF/vrf-func-ffi/src"
 )
 
@@ -125,36 +126,22 @@ func keyRotation(t types.TimeSlot, tPrime types.TimeSlot, safroleState types.Sta
 // KeyRotate rotates the keys
 // Update the state with the new Safrole state
 // (6.13)
-func KeyRotate() {
+func KeyRotate(e types.TimeSlot, ePrime types.TimeSlot) error {
 	s := store.GetInstance()
 
 	// Get prior state
 	priorState := s.GetPriorStates()
 
-	// Get previous time slot index
-	tau := priorState.GetTau()
-
-	// Get current time slot
-	now := time.Now().UTC()
-	timeInSecond := uint64(now.Sub(types.JamCommonEra).Seconds())
-	tauPrime := types.TimeSlot(timeInSecond / uint64(types.SlotPeriod))
-
-	// Execute key rotation
-	// newSafroleState := keyRotation(tau, tauPrime, priorState.GetState())
-	e := GetEpochIndex(tau)
-	ePrime := GetEpochIndex(tauPrime)
-
+	log.Println("e", e, "ePrime", ePrime)
 	if ePrime > e {
-		z, zErr := UpdateBandersnatchKeyRoot(priorState.GetGammaK())
-		if zErr != nil {
-			fmt.Printf("Error updating Bandersnatch key root: %v\n", zErr)
-			return
-		}
-
 		// Update state to posterior state
 		s.GetPosteriorStates().SetGammaK(ReplaceOffenderKeys(priorState.GetIota()))
 		s.GetPosteriorStates().SetKappa(priorState.GetGammaK())
 		s.GetPosteriorStates().SetLambda(priorState.GetKappa())
+		z, zErr := UpdateBandersnatchKeyRoot(s.GetPosteriorStates().GetGammaK())
+		if zErr != nil {
+			return fmt.Errorf("error updating Bandersnatch key root: %v", zErr)
+		}
 		s.GetPosteriorStates().SetGammaZ(z)
 	} else {
 		s.GetPosteriorStates().SetGammaK(priorState.GetGammaK())
@@ -162,4 +149,69 @@ func KeyRotate() {
 		s.GetPosteriorStates().SetLambda(priorState.GetLambda())
 		s.GetPosteriorStates().SetGammaZ(priorState.GetGammaZ())
 	}
+	return nil
+}
+
+// Outer Safrole function
+// I made this function return ErrorCode only
+func OuterUsedSafrole() *types.ErrorCode {
+	s := store.GetInstance()
+
+	// --- STEP 1 Get Epoch and Slot for safrole --- //
+	var (
+		tau            = s.GetPriorStates().GetTau()
+		tauPrime       = s.GetPosteriorStates().GetTau()
+		e, m           = R(tau)
+		ePrime, mPrime = R(tauPrime)
+	)
+	log.Println(e, ePrime, m, mPrime)
+	// prior time slot must be less than posterior time slot
+	if tau >= tauPrime {
+		errCode := SafroleErrorCode.BadSlot
+		return &errCode
+	}
+	// --- STEP 2 Update Entropy123 --- //
+	// (GP 6.23)
+	UpdateEntropy(e, ePrime)
+
+	// --- STEP 3 safrole.go (GP 6.2, 6.13, 6.14) --- //
+	// (6.2, 6.13, 6.14)
+	// This function will update GammaK, GammaZ, Lambda, Kappa
+	err := KeyRotate(e, ePrime)
+	if err != nil {
+		log.Println("keyRotateErr:", err)
+	}
+
+	// (GP 6.22)
+	// err = UpdateEtaPrime0()
+	// if err != nil {
+	// 	log.Println("UpdateEtaPrime0Err:", err)
+	// }
+
+	// (GP 6.17) // This will be used to write H_v to new header
+	// UpdateHeaderEntropy()
+
+	// --- slot_key_sequence.go (GP 6.25, 6.26) --- //
+	UpdateSlotKeySequence(e, ePrime, m)
+
+	// --- STEP 4 Check TicketExtrinsic --- //
+	// --- extrinsic_tickets.go (GP 6.30~6.34) --- //
+	EtErrCode := CreateNewTicketAccumulator()
+	if EtErrCode != nil {
+		return EtErrCode
+	}
+	// (GP 6.28)
+	CreateWinningTickets(e, ePrime, m, mPrime)
+
+	// --- sealing.go (GP 6.15~6.24) --- //
+	err = SealingHeader()
+	if err != nil {
+		log.Println("SealingHeaderErr:", err)
+	}
+
+	// --- markers.go (GP 6.27, 6.28) --- //
+	// (GP 6.27)
+	CreateEpochMarker(e, ePrime)
+
+	return nil
 }
