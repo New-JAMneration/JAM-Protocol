362fd48fb98da366a459285058d439f634aa8642
terry.hung <terryhung1228@gmail.com>
2025-07-20 13:08:41 +0800

    feat: support importBlock in FuzzService (#604)

--- internal/fuzz/service.go
+++ internal/fuzz/service.go
@@ -1,8 +1,11 @@
 package fuzz
 
 import (
-	"log"
+	"errors"
+	"fmt"
 
+	"github.com/New-JAMneration/JAM-Protocol/internal/blockchain"
+	"github.com/New-JAMneration/JAM-Protocol/internal/stf"
 	"github.com/New-JAMneration/JAM-Protocol/internal/types"
 )
 
@@ -15,6 +18,7 @@ type FuzzService interface {
 
 type FuzzServiceStub struct {
 	// TODO: fill in whatever dependency you need
+	Blockchain blockchain.Blockchain
 }
 
 func (s *FuzzServiceStub) Handshake(peerInfo PeerInfo) (PeerInfo, error) {
@@ -33,8 +37,31 @@ func (s *FuzzServiceStub) Handshake(peerInfo PeerInfo) (PeerInfo, error) {
 }
 
 func (s *FuzzServiceStub) ImportBlock(block types.Block) (StateRoot, error) {
-	// TODO
-	return StateRoot{}, ErrNotImpl
+	// Do some validation on the block here
+	if s.Blockchain.GetLatestBlock().Header.Parent != block.Header.Parent {
+		return StateRoot{}, fmt.Errorf("parent mismatch: got %x, want %x", s.Blockchain.GetLatestBlock().Header.Parent, block.Header.Parent)
+	}
+
+	if s.Blockchain.GetLatestBlock().Header.ParentStateRoot != block.Header.ParentStateRoot {
+		return StateRoot{}, fmt.Errorf("state_root mismatch: got %x, want %x", s.Blockchain.GetLatestBlock().Header.ParentStateRoot, block.Header.ParentStateRoot)
+	}
+
+	// Store the block in the blockchain (into redis)S
+	blockHash := block.Header.Parent
+	err := s.Blockchain.StoreBlockByHash(blockHash, &block)
+	if err != nil {
+		return StateRoot{}, err
+	}
+
+	// Run the STF and get the state root
+	err = stf.RunSTF()
+	if err != nil {
+		return StateRoot{}, err
+	}
+
+	latestBlock := s.Blockchain.GetLatestBlock()
+
+	return StateRoot(latestBlock.Header.ParentStateRoot), nil
 }
 
 func (s *FuzzServiceStub) SetState(header types.Header, state State) (StateRoot, error) {
