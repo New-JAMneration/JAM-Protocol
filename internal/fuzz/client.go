ca944883c1b521e5b7c4ab7756672651f8adaf7f
Alex Tsay <alex.m.tsay@gmail.com>
2025-07-26 18:14:28 -0400

    refactor: add strong type to message.

--- internal/fuzz/client.go
+++ internal/fuzz/client.go
@@ -6,7 +6,6 @@ import (
 	"github.com/New-JAMneration/JAM-Protocol/internal/types"
 )
 
-// TODO
 type FuzzClient struct {
 	conn net.Conn
 }
@@ -28,47 +27,105 @@ func (c *FuzzClient) Close() error {
 	return c.conn.Close()
 }
 
-func (c *FuzzClient) Handshake(peerInfo PeerInfo) (PeerInfo, error) {
-	m := Message{
-		Type:    MessageType_PeerInfo,
-		payload: &peerInfo,
+func (c *FuzzClient) makeRequest(req Message) (Message, error) {
+	reqBytes, err := req.MarshalBinary()
+	if err != nil {
+		return Message{}, err
 	}
 
-	req, err := m.MarshalBinary()
+	_, err = c.conn.Write(reqBytes)
 	if err != nil {
-		return PeerInfo{}, err
-	}
-
-	_, err = c.conn.Write(req)
-	if err != nil {
-		return PeerInfo{}, err
+		return Message{}, err
 	}
 
 	var resp Message
+
 	_, err = resp.ReadFrom(c.conn)
 	if err != nil {
-		return PeerInfo{}, err
+		return Message{}, err
 	}
 
-	info, err := resp.PeerInfo()
+	return resp, nil
+}
+
+func (c *FuzzClient) Handshake(peerInfo PeerInfo) (PeerInfo, error) {
+	req := Message{
+		Type:     MessageType_PeerInfo,
+		PeerInfo: &peerInfo,
+	}
+
+	resp, err := c.makeRequest(req)
 	if err != nil {
-		return PeerInfo{}, err
+		return PeerInfo{}, nil
 	}
 
-	return *info, nil
+	if resp.Type != MessageType_PeerInfo {
+		return PeerInfo{}, ErrInvalidMessageType
+	}
+
+	return *resp.PeerInfo, nil
 }
 
 func (c *FuzzClient) ImportBlock(block types.Block) (types.StateRoot, error) {
-	// TODO
-	return types.StateRoot{}, ErrNotImpl
+	payload := ImportBlock(block)
+
+	req := Message{
+		Type:        MessageType_ImportBlock,
+		ImportBlock: &payload,
+	}
+
+	resp, err := c.makeRequest(req)
+	if err != nil {
+		return types.StateRoot{}, err
+	}
+
+	if resp.Type != MessageType_StateRoot {
+		return types.StateRoot{}, ErrInvalidMessageType
+	}
+
+	return types.StateRoot(*resp.StateRoot), nil
 }
 
 func (c *FuzzClient) SetState(header types.Header, state types.StateKeyVals) (types.StateRoot, error) {
-	// TODO
-	return types.StateRoot{}, ErrNotImpl
+	payload := SetState{
+		Header: header,
+		State:  state,
+	}
+
+	req := Message{
+		Type:     MessageType_SetState,
+		SetState: &payload,
+	}
+
+	resp, err := c.makeRequest(req)
+	if err != nil {
+		return types.StateRoot{}, err
+	}
+
+	if resp.Type != MessageType_StateRoot {
+		return types.StateRoot{}, ErrInvalidMessageType
+	}
+
+	return types.StateRoot(*resp.StateRoot), nil
 }
 
 func (c *FuzzClient) GetState(hash types.HeaderHash) (types.StateKeyVals, error) {
+	payload := GetState(hash)
+
+	req := Message{
+		Type:     MessageType_GetState,
+		GetState: &payload,
+	}
+
+	resp, err := c.makeRequest(req)
+	if err != nil {
+		return types.StateKeyVals{}, err
+	}
+
+	if resp.Type != MessageType_State {
+		return types.StateKeyVals{}, ErrInvalidMessageType
+	}
+
 	// TODO
-	return types.StateKeyVals{}, ErrNotImpl
+	return types.StateKeyVals(*resp.State), nil
 }
