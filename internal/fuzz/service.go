5cbf50f19eac6a4edf3c84fa08893e513743d4ae
Alex Tsay <alex.m.tsay@gmail.com>
2025-07-26 15:34:58 -0400

    refactor: updated value types in the service interface.
    This is to better match the types we use internally.

--- internal/fuzz/service.go
+++ internal/fuzz/service.go
@@ -12,9 +12,9 @@ import (
 
 type FuzzService interface {
 	Handshake(PeerInfo) (PeerInfo, error)
-	ImportBlock(types.Block) (StateRoot, error)
-	SetState(types.Header, State) (StateRoot, error)
-	GetState(types.HeaderHash) (State, error)
+	ImportBlock(types.Block) (types.StateRoot, error)
+	SetState(types.Header, types.StateKeyVals) (types.StateRoot, error)
+	GetState(types.HeaderHash) (types.StateKeyVals, error)
 }
 
 type FuzzServiceStub struct {
@@ -32,35 +32,35 @@ func (s *FuzzServiceStub) Handshake(peerInfo PeerInfo) (PeerInfo, error) {
 	return response, nil
 }
 
-func (s *FuzzServiceStub) ImportBlock(block types.Block) (StateRoot, error) {
+func (s *FuzzServiceStub) ImportBlock(block types.Block) (types.StateRoot, error) {
 	// Do some validation on the block here
 	if s.Blockchain.GetLatestBlock().Header.Parent != block.Header.Parent {
-		return StateRoot{}, fmt.Errorf("parent mismatch: got %x, want %x", s.Blockchain.GetLatestBlock().Header.Parent, block.Header.Parent)
+		return types.StateRoot{}, fmt.Errorf("parent mismatch: got %x, want %x", s.Blockchain.GetLatestBlock().Header.Parent, block.Header.Parent)
 	}
 
 	if s.Blockchain.GetLatestBlock().Header.ParentStateRoot != block.Header.ParentStateRoot {
-		return StateRoot{}, fmt.Errorf("state_root mismatch: got %x, want %x", s.Blockchain.GetLatestBlock().Header.ParentStateRoot, block.Header.ParentStateRoot)
+		return types.StateRoot{}, fmt.Errorf("state_root mismatch: got %x, want %x", s.Blockchain.GetLatestBlock().Header.ParentStateRoot, block.Header.ParentStateRoot)
 	}
 
 	// Store the block in the blockchain (into redis)S
 	blockHash := block.Header.Parent
 	err := s.Blockchain.StoreBlockByHash(blockHash, &block)
 	if err != nil {
-		return StateRoot{}, err
+		return types.StateRoot{}, err
 	}
 
 	// Run the STF and get the state root
 	err = stf.RunSTF()
 	if err != nil {
-		return StateRoot{}, err
+		return types.StateRoot{}, err
 	}
 
 	latestBlock := s.Blockchain.GetLatestBlock()
 
-	return StateRoot(latestBlock.Header.ParentStateRoot), nil
+	return latestBlock.Header.ParentStateRoot, nil
 }
 
-func (s *FuzzServiceStub) SetState(header types.Header, state types.StateKeyVals) (StateRoot, error) {
+func (s *FuzzServiceStub) SetState(header types.Header, state types.StateKeyVals) (types.StateRoot, error) {
 	storeInstance := store.GetInstance()
 
 	for _, value := range state {
@@ -136,10 +136,10 @@ func (s *FuzzServiceStub) SetState(header types.Header, state types.StateKeyVals
 
 	}
 	// TODO
-	return StateRoot{}, ErrNotImpl
+	return types.StateRoot{}, ErrNotImpl
 }
 
-func (s *FuzzServiceStub) GetState(hash types.HeaderHash) (State, error) {
+func (s *FuzzServiceStub) GetState(hash types.HeaderHash) (types.StateKeyVals, error) {
 	// TODO
-	return State{}, ErrNotImpl
+	return types.StateKeyVals{}, ErrNotImpl
 }
