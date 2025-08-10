dc8fafcc27057db47adda540c74f080a628f398d
yu2C <c2001413@gmail.com>
2025-08-10 22:12:08 +0800

    refactor: add test for genesis

--- internal/utilities/merklization/parse_state_key_vals_test.go
+++ internal/utilities/merklization/parse_state_key_vals_test.go
@@ -44,6 +44,55 @@ func TestStateKeyValsToState(t *testing.T) {
 			// Parse the state keyvals
 			// TODO: prestate or poststate
 			_, err = StateKeyValsToState(traceTestCase.PreState.KeyVals)
+			if err != nil {
+				t.Errorf("Error parsing state keyvals: %v", err)
+			}
+
+			// TODO: 正確答案從哪裡拿?
+		}
+	}
+}
+
+func TestStateKeyValsToStateGenesis(t *testing.T) {
+	dirNames := []string{
+		"fallback",
+		// "preimages",
+		// "preimages_light",
+		// "safrole",
+		// "storage",
+		// "storage_light",
+	}
+
+	for _, dirName := range dirNames {
+		dir := filepath.Join("..", utilities.JAM_TEST_VECTORS_DIR, "traces", dirName)
+
+		fileNames, err := utilities.GetTargetExtensionFiles(dir, utilities.BIN_EXTENTION)
+		if err != nil {
+			t.Errorf("Error getting files from directory %s: %v", dir, err)
+			continue
+		}
+
+		for _, fileName := range fileNames {
+			if fileName != "genesis.bin" { // only genesis
+				continue
+			}
+			filePath := filepath.Join(dir, fileName)
+			println(filePath)
+
+			// Read the bin file
+			traceTestCase := &jamtests_trace.Genesis{}
+			err := utilities.GetTestFromBin(filePath, traceTestCase)
+			if err != nil {
+				t.Errorf("Error reading file %s: %v", filePath, err)
+				continue
+			}
+
+			// Parse the state keyvals
+			// TODO: prestate or poststate
+			_, err = StateKeyValsToState(traceTestCase.State.KeyVals)
+			if err != nil {
+				t.Errorf("Error parsing state keyvals: %v", err)
+			}
 
 			// TODO: 正確答案從哪裡拿?
 		}
