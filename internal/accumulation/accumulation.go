8ef1a4d1d1ca4ab43478a73476a7ed604d776619
HanaYukii <islu245777@gmail.com>
2025-10-28 02:10:01 +0800

    fix: update Designate with vStar to match PVM expectations

--- internal/accumulation/accumulation.go
+++ internal/accumulation/accumulation.go
@@ -420,6 +420,7 @@ func ParallelizedAccumulation(input ParallelizedAccumulationInput) (output Paral
 	vStar := singleOutput.PartialStateSet.Designate
 	zPrime := singleOutput.PartialStateSet.AlwaysAccum
 	aPrime := make(types.ServiceIdList, types.CoresCount)
+	singleInput.PartialStateSet.Designate = vStar
 
 	// ∀c ∈ NC ∶ a′c = ((∆1(o, w, f, a∗c )o)a)c
 	if len(aStar) != types.CoresCount {
@@ -536,6 +537,7 @@ func SingleServiceAccumulation(input SingleServiceAccumulationInput) (output Sin
 	pvmResult := PVM.Psi_A(e, tauPrime, s, g, i, eta0, storageKeyVal)
 	fmt.Printf("service: %d, pvmResult.Designate: %d\n", s, pvmResult.PartialStateSet.Designate)
 	store.GetInstance().SetStorageKeyVals(pvmResult.StorageKeyVal)
+
 	// Collect PVM results as output
 	{
 		output.AccumulationOutput = pvmResult.Result
