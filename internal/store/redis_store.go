8844d80a6ff968d2e0ef6a9e373d70d3debb9acd
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2025-11-05 22:24:16 +0000

    revert unncessary changes

--- internal/store/redis_store.go
+++ internal/store/redis_store.go
@@ -2,12 +2,14 @@ package store
 
 import (
 	"context"
+	"encoding/hex"
 	"errors"
 	"log"
 	"os"
 	"sync"
 
 	"github.com/New-JAMneration/JAM-Protocol/config"
+	"github.com/New-JAMneration/JAM-Protocol/internal/types"
 	"github.com/alicebob/miniredis/v2"
 )
 
@@ -52,6 +54,35 @@ func GetRedisBackend() (*RedisBackend, error) {
 		return nil, errors.New("redis backend is not initialized")
 	}
 	return globalRedisBackend, nil
+}
+
+func genGenesisBlock() *types.Block {
+	hash := "5c743dbc514284b2ea57798787c5a155ef9d7ac1e9499ec65910a7a3d65897b7"
+	byteArray, _ := hex.DecodeString(hash)
+	genesisBlock := types.Block{
+		Header: types.Header{
+			// hash string to jamTypes.HeaderHash
+			Parent:          types.HeaderHash(byteArray),
+			ParentStateRoot: types.StateRoot{},
+			ExtrinsicHash:   types.OpaqueHash{},
+			Slot:            0,
+			EpochMark:       nil,
+			TicketsMark:     nil,
+			OffendersMark:   types.OffendersMark{},
+			AuthorIndex:     0,
+			EntropySource:   types.BandersnatchVrfSignature{},
+			Seal:            types.BandersnatchVrfSignature{},
+		},
+		Extrinsic: types.Extrinsic{
+			Tickets:    types.TicketsExtrinsic{},
+			Preimages:  types.PreimagesExtrinsic{},
+			Guarantees: types.GuaranteesExtrinsic{},
+			Assurances: types.AssurancesExtrinsic{},
+			Disputes:   types.DisputesExtrinsic{},
+		},
+	}
+
+	return &genesisBlock
 }
 
 func genInitHashSegmentMap() map[string]string {
