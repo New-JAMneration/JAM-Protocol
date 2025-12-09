ac69dbbb66b6d2aad4aac42032851b5867d1aac0
Yu Chun Tsao <tsao84672776@gmail.com>
2025-12-09 22:37:12 +0800

    refactor(docker): unify Dockerfiles and simplify scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,7 @@ docker build \
   --build-arg GP_VERSION=${GP_VERSION} \
   --build-arg TARGET_VERSION=${TARGET_VERSION} \
   --build-arg OUTPUT=${OUTPUT} \
-  -t build-new-jamneration-target -f docker/build.Dockerfile .
-docker create --name tempcontainer build-new-jamneration-target
+  -t new-jamneration-target -f docker/Dockerfile .
+docker create --name tempcontainer new-jamneration-target
 docker cp tempcontainer:/build .
 docker rm tempcontainer
